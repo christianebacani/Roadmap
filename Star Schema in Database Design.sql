eb95e0d0d44dbf9d1461b1baa1efb8c7c19d41e3
christianebacani <121742592+christianebacani@users.noreply.github.com>
2024-09-16 04:28:38 +0800

    Update Star Schema in Database Design.sql

--- Star Schema in Database Design.sql
+++ Star Schema in Database Design.sql
@@ -1,11 +1,15 @@
 -- Database Name/Any Database
 USE practicedb;
 
+
 -- Hello! My name is Christiane A. Bacani a Data/DevOps Engineer
+
 
 -- Warehouse Modeling
 -- Star Schema
 
+
+-- Drop Tables
 DROP TABLE IF EXISTS borrows;
 DROP TABLE IF EXISTS members;
 DROP TABLE IF EXISTS dates;
@@ -13,23 +17,31 @@ DROP TABLE IF EXISTS book_shelves;
 DROP TABLE IF EXISTS libraries;
 
 
+-- Dimension Table for Fact Entity
 CREATE TABLE libraries(library_id INT PRIMARY KEY,
 			library TEXT);
-                        
+
+
+-- Dimension Table for Fact Entity
 CREATE TABLE book_shelves(shelf_id INT PRIMARY KEY,
 			    shelf TEXT,
                             book_id INT,
                             book TEXT);
-                    
+
+
+-- Dimension Table for Fact Entity                    
 CREATE TABLE dates(date_id INT PRIMARY KEY,
 		    month INT,
                     day MEDIUMINT, 
                     year MEDIUMINT);
-			
+
+
+-- Dimension Table for Fact Entity
 CREATE TABLE members(member_id INT PRIMARY KEY,
 		     member TEXT);
-                        
 
+                        
+-- Borrows Fact Entity
 CREATE TABLE borrows(member_id INT,
                         date_id INT,
                         shelf_id INT,
