5195d99cfe1e085e564605118947997b2c04becd
christianebacani <christianbacani581@gmail.com>
2024-11-16 18:27:41 +0800

    Modified the script linux_file_sys_cli_manager.sh inside Bash Scripts Directory

--- Bash Scripts/linux_file_sys_cli_manager.sh
+++ Bash Scripts/linux_file_sys_cli_manager.sh
@@ -1,276 +1,267 @@
 #!/bin/bash
 
+# Linux File System CLI Manager
 
-# CLI for Managing Directories in Linux
-
+# TODO : Rename the variables to follow global naming conventions
 
 # Main Function
 function main(){
-
-	echo "Changing/Managing Directory Bash Script!"
-
-	# Press enter to start
 	read -p "Press any key to start: " START
 
-	# Execute the function to manage directory
+	clear
 	manage_dir
 }
 
 
 # Change Directory Function
 function manage_dir(){
+	function reload(){
+		clear
+		ls -l
+	}
 
 	while true
 	do
-	 	# Options to choose of different directory
-	        echo "Directories:"
-		echo "1.) parent"
-        	echo "2.) root"
+		printf "\n"
+		echo "-----------------------------------------------------------------"
+		printf "\t\tDIRECTORIES\n\n"
+
+		echo "1.) Parent root"
+        	echo "2.) child root"
         	echo "3.) scripts"
-		echo "4.) python_scripts"
-        	echo "5.) playground"
-		echo "6.) personal"
-        	echo "7.) exit"
+        	echo "4.) playground"
+			echo "5.) personal"
+        	echo "6.) exit"
 
 		read -p "Enter your option here : " OPTION
 
-		# Check if user's choice is valid
-		if [[ "$OPTION" -ge 1 ]] && [[ "$OPTION" -le 7 ]]
-		then
-			# Change directory and list all the script inside the directory
-			if [[ "$OPTION" -eq 1 ]]
-			then
-				cd /
+		# Check if user's option is invalid
+		if [[ "$OPTION" -le 0 ]] && [[ "$OPTION" -ge 7 ]]; then 
+			reload
+			continue
 
-			elif [[ "$OPTION" -eq 2 ]]
-			then
-				cd /root
-
-			elif [[ "$OPTION" -eq 3 ]]
-			then
-				cd /root/scripts
-
-			elif [[ "$OPTION" -eq 4 ]]
-			then
-				cd /root/scripts/python_scripts
-
-			elif [[ "$OPTION" -eq 5 ]]
-			then
-				d /root/scripts/playground
-
-			elif [[ "$OPTION" -eq 6 ]]
-			then
-				cd /root/scripts/playground/personal
-
-			else
-				# Exit message
-
-				echo "Exiting..."
-
-				break
-
-			fi
+		elif [[ "$OPTION" =~ [A-Za-z]+ ]]; the 
+			reload 
+			continue
+	
+		fi
 
 
-			# Clear past CLI Interactions and display the scripts in long listing format in the current directory
-
-			clear
-			ls -l
-
-			# Execute manage directory function
-			manage_directory
+		# Change directory and list all the script inside the directory
+		if [[ "$OPTION" -eq 1 ]]; then
+			cd /
 
 
-		# Ask user again if it's invalid choice
+		elif [[ "$OPTION" -eq 2 ]]; then
+			cd /root
+
+
+		elif [[ "$OPTION" -eq 3 ]]; then
+			cd /root/scripts
+
+
+		elif [[ "$OPTION" -eq 4 ]]; then
+			cd /root/scripts/playground
+
+
+		elif [[ "$OPTION" -eq 5 ]]; then
+			cd /root/scripts/playground/personal
+
 		else
-			echo "Invalid input!"
+			# Exit message
+			printf "\n\t\tEXITING\n"
+
+			echo "-----------------------------------------------------------------"
+			cd /root
+			reload
+
+			break
 
 
 		fi
 
-	done
+		reload
+		manage_directory
 
+	done
 }
 
 
 # Modifying Directory Function
 function manage_directory(){
+	while true; do
+		printf "\n"
+		echo "-----------------------------------------------------------------"
+		printf "\t\tCOMMANDS\n\n"
 
-	while true
-	do
-		# Options
-		echo "Choose Directory Commands"
-		echo "1.) Remove script"
-		echo "2.) Rename script"
-		echo "3.) Moving script"
-		echo "4.) Execute script"
-		echo "5.) Edit/Create a script"
-		echo "6.) Change permission for a script"
-		echo "7.) Exit Interface"
+		echo "1.) Remove"
+		echo "2.) Rename"
+		echo "3.) Moving"
+		echo "4.) Execute"
+		echo "5.) Create/Modify"
+		echo "6.) Change permission"
+		echo "7.) Exit"
 
-		read -p "Choose your commands here : " COMMAND # User Choose any command
+		read -p "Choose your commands here : " COMMAND
 
 		# Check if the user's choice of commannd is valid
-		if [[ "$COMMAND" -ge 1 ]] && [[ "$COMMAND" -le 7 ]]
-		then
-			# Manage script
-
-			if [[ "$COMMAND" -ge 1 ]] && [[ "$COMMAND" -le 6 ]]
-			then
-
-				# User enters the script name
-				read -p "Enter the script name here : " SCRIPT
-
-				# Check if the specific script exist
-				if [[ -n "$SCRIPT" ]]
-				then
-
-					# Execute the manage script function
-					manage_script "$SCRIPT" "$COMMAND"
-
-					# Command to clear the past interactions of CLI Interface in shell environment
-					clear
-
-					# Display all the scripts inside the directory in long listing format
-					ls -l
-
-				else
-					echo "The ${SCRIPT} doesn't exist! Please try again."
-
-				fi
-
-			# Exit interface
-			else
-
-				echo "Exiting..."
-				break
-
-			fi
-
-
-		else
-			# Display a message when user enters invalid prompt
-			echo "Invalid input!"
+		if [[ "$COMMAND" -le 0 ]] && [[ "$COMMAND" -ge 8 ]] || [[ "$COMMAND" =~ [A-Za-z]+ ]]; then
+			reload
+			continue
 
 		fi
+
+
+		# Exit interface
+		if [[ "$COMMAND" -eq 7 ]]; then
+			break
+
+		fi
+
+
+		read -p "Enter the script name here : " SCRIPT
+
+		manage_script "$SCRIPT" "$COMMAND"
+		reload
+
 	done
 }
 
 
 # Function to manage script
 function manage_script(){
-
-	local LOCAL_SCRIPT="$1" # Argument of the user's script choice
-
-	local LOCAL_COMMAND="$2" # Argument of the user's option
-
+	local LOCAL_SCRIPT="$1"
+	local LOCAL_COMMAND="$2"
 
 
 	# Remove a script
-
-	if [[ "$LOCAL_COMMAND" -eq 1 ]]
-	then
-
-		# Command to remove script from the directory
+	if [[ -f "$LOCAL_SCRIPT" ]] && [[ "$LOCAL_COMMAND" -eq 1 ]]; then
                 rm "$LOCAL_SCRIPT"
 
 
 	# Renaming script
-
-	elif [[ "$LOCAL_COMMAND" -eq 2 ]]
-	then
-
-		# User enters the new script name
-		read -p "Enter the new script name : " NEW_SCRIPT
-
-		if [[ -n "$NEW_SCRIPT" ]] # Check if the new script is not null
-		then
-			# Command to rename the script
-			mv "$LOCAL_SCRIPT" "$NEW_SCRIPT"
-
-		else
-			# Invalid message when user writes invalid script name
-			echo "Invalid file name!"
-
-		fi
+	elif [[ -f  "$LOCAL_SCRIPT" ]] && [[ "$LOCAL_COMMAND" -eq 2 ]]; then
+		renameScript "$1"
 
 
 	# Moving script to another directory
-
-	elif [[ "$LOCAL_COMMAND" -eq 3 ]]
-	then
-
-		# User enters the directory to store the script
-		read -p "Enter a directory to store your specific script : " DIRECTORY
-
-
-		# Check if the directory is a valid directory or existing
-		if [[ -e "$DIRECTORY" ]]
-		then
-			# Command to move script to another directory
-			mv "$LOCAL_SCRIPT" "$DIRECTORY"
-
-
-		else
-			# Display message when user writes invalid directory
-			echo "The ${DIRECTORY} doesn't exist! Please try again."
-
-		fi
-
+	elif [[ -f "$LOCAL_SCRIPT" ]] && [[ "$LOCAL_COMMAND" -eq 3 ]]; then
+		moveScript "$LOCAL_SCRIPT"
 
 	# Execute script/s
-
-	elif [[ "$LOCAL_COMMAND" -eq 4 ]]
-	then
-		# Command to execute script
-
-		. "$LOCAL_SCRIPT"
-
-		# Ask user to reload the interface
+	elif [[ -f "$LOCAL_SCRIPT" ]] && [[ "$LOCAL_COMMAND" -eq 4 ]]; then
+		bash "$LOCAL_SCRIPT"
 		read -p "Press any key to load the Command Line Interface again: "
 
 
-	# Create or edit script/s
+	# Create/Edit script/s
 	elif [[ "$LOCAL_COMMAND" -eq 5 ]]
 	then
-		# Edit or open a new script
-
-		nano "$LOCAL_SCRIPT"
+		createOrModifyScript "$LOCAL_SCRIPT"
 
 
 	# Change permissions
 	else
-		# Permission options
 
-		echo "Permission options"
-		echo "Read (r)"
-		echo "Write (w)"
-		echo "Execute (x)"
-
-
-		# User writes the permission
-		read -p "Enter your choice here based on the letter of command : " PERMISSION
-
-		if [[ "$PERMISSION" == "r" ]] || [[ "$PERMISSION" == "w" ]] || [[ "$PERMISSION" == "x" ]]
-		then
-
-			# Command to change permission
-			chmod u+"${PERMISSION}" "$LOCAL_SCRIPT"
-
-		else
-			# Display a message when user enters invalid permission
-
-			echo "Invalid permission!"
-
-
-		# Ask user to press any key to load the interface again
-
-		read -p "Press any key to load the interface again : "
-
-
-		fi
+		changeScriptPermission "$LOCAL_SCRIPT"
 
 	fi
+}
+
+
+
+# Rename script
+function renameScript(){
+	local LOCAL_SCRIPT="$1"
+
+	read -p "Enter the new script name : " NEW_SCRIPT
+
+	if [[ -n "$NEW_SCRIPT" ]]; then
+		mv "$LOCAL_SCRIPT" "$NEW_SCRIPT"
+
+	else
+		echo "Invalid file name!"
+
+	fi
+
+}
+
+
+
+# Move script
+function moveScript(){
+	local LOCAL_SCRIPT="$1"
+
+	read -p "Enter a directory to store your script : " DIRECTORY
+
+
+	if [[ -d "$DIRECTORY" ]]; then
+		mv "$LOCAL_SCRIPT" "$DIRECTORY"
+
+	else
+		echo "The ${DIRECTORY} doesn't exist! Please try again."
+
+	fi
+}
+
+
+
+# Create/Modify script
+function createOrModifyScript(){
+	local LOCAL_SCRIPT="$1"
+
+	if [[ -n "$LOCAL_SCRIPT" ]]; then
+		nano "$LOCAL_SCRIPT"
+
+	else
+		echo "Invalid Input! Please try again."
+		read -p "Press enter to load the interface again : "
+
+	fi
+}
+
+
+
+# Changing script permission
+function changeScriptPermission(){
+	function invalidInput(){
+		echo "Invalid input! Please try again."
+		read -p "Press any key to load the interface again : "
+
+	}
+
+	local LOCAL_SCRIPT="$1"
+
+	echo "-----------------------------------------------------------------"
+	printf "\t\tChange Permissions\n\n"
+
+	echo "Read (r)"
+	echo "Write (w)"
+	echo "Execute (x)"
+
+	read -p "Change file permission here based on the acronyms : " PERMISSION
+
+	PERMISSION=${PERMISSION,,}
+
+	if [[ "$PERMISSION" =~ [0-9]+ ]]; then
+		invalidInput
+
+	elif [[ "$PERMISSION" == "r" ]]; then
+		chmod u+r "$LOCAL_SCRIPT"
+
+
+	elif [[ "$PERMISSION" == "w" ]]; then
+		chmod u+w "$LOCAL_SCRIPT"
+
+
+	elif [[ "$PERMISSION" == "x" ]]; then
+		chmod u+x "$LOCAL_SCRIPT"
+
+	else
+		invalidInput
+
+	fi
+
 }
 
 
