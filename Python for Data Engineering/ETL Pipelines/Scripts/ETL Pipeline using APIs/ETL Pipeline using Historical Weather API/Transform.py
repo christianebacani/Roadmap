c3da99725cf615b785c2317b6f9e54ee2e42c204
Christiane Rhely Joselle Bacani <121742592+christianebacani@users.noreply.github.com>
2025-01-09 19:50:07 +0800

    Update Transform.py

--- Python for Data Engineering/ETL Pipelines/Scripts/ETL Pipeline using APIs/ETL Pipeline using Historical Weather API/Transform.py
+++ Python for Data Engineering/ETL Pipelines/Scripts/ETL Pipeline using APIs/ETL Pipeline using Historical Weather API/Transform.py
@@ -5,9 +5,9 @@ import pandas as pd
 def transform(extractedDailyWeatherDataFrom2024):
     # Dictionary to store daily weather metadata
     dailyWeatherParameterDataDict = {'time' : [], 'weather_code' : [], 'temperature_2m_max' : [], 'temperature_2m_min' : [], 'temperature_2m_mean' : [], 
-                                                'apparent_temperature_max' : [], 'apparent_temperature_min' : [], 'apparent_temperature_mean' : [], 'sunrise' : [], 'sunset' : [], 
-                                                'daylight_duration' : [], 'sunshine_duration' : [], 'precipitation_sum' : [], 'rain_sum' : [], 'snowfall_sum' : [], 
-                                                'precipitation_hours' : [], 'wind_speed_10m_max' : [], 'wind_gusts_10m_max' : [], 'wind_direction_10m_dominant' : [], 'shortwave_radiation_sum' : [], 'et0_fao_evapotranspiration' : []}
+                                      'apparent_temperature_max' : [], 'apparent_temperature_min' : [], 'apparent_temperature_mean' : [], 'sunrise' : [], 'sunset' : [], 
+                                      'daylight_duration' : [], 'sunshine_duration' : [], 'precipitation_sum' : [], 'rain_sum' : [], 'snowfall_sum' : [], 
+                                      'precipitation_hours' : [], 'wind_speed_10m_max' : [], 'wind_gusts_10m_max' : [], 'wind_direction_10m_dominant' : [], 'shortwave_radiation_sum' : [], 'et0_fao_evapotranspiration' : []}
     
     # Daily unit columns
     dailyUnitColumns = []
