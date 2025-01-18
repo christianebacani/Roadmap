55bc4433f89872051fa7b7be69cd4a75140f7241
christianebacani <christianbacani581@gmail.com>
2025-01-19 00:33:18 +0800

    Update Coding Challenges in Python (Rename the folder name and restructure the subfolders)

--- Python for Data Engineering/ETL Pipelines/Scripts/ETL Pipeline Scripts using APIs/ETL Pipeline using Nba API/Load.py
+++
@@ -1,8 +0,0 @@
-# Load
-
-def load(playersData):
-    targetFilepath = 'Target Data Files\\Nba Players Data for the Season 2024-2025\\players_data_season_2024.csv'
-    playersData.to_csv(targetFilepath, index=False)
-    
-    return playersData
-
