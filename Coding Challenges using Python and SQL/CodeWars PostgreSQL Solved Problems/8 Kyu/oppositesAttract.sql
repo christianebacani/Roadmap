c193015687d09714599d2bf77842878f8c97d5b6
christianebacani <christianbacani581@gmail.com>
2025-03-01 14:09:37 +0800

    Added new postgresql file

---
+++ Coding Challenges using Python and SQL/CodeWars PostgreSQL Solved Problems/8 Kyu/oppositesAttract.sql
@@ -0,0 +1,12 @@
+-- 8 Kyu
+-- Opposites Attract
+
+SELECT
+  flower1,
+  flower2,
+  CASE
+    WHEN (flower1 % 2 != 0 AND flower2 % 2 = 0) OR (flower1 % 2 = 0 AND flower2 % 2 != 0) THEN True
+    ELSE False
+  END AS res
+FROM
+  love;
