c98c2bd4e23ec7a13877931903a5e62ba9862f23
christianebacani <christianbacani581@gmail.com>
2025-03-01 19:27:37 +0800

    added new python file

---
+++ Coding Challenges using Python and SQL/LeetCode Python Solved Problems/Easy Level/applyOperationsToAnArray.py
@@ -0,0 +1,22 @@
+# 2460.) Apply Operations to an Array
+# Categories : Array, Two Pointers, Simulation
+
+class Solution:
+    def applyOperations(self, nums: list[int]) -> list[int]:
+
+        for i in range(1, len(nums)):
+            if nums[i - 1] == nums[i]:
+                nums[i - 1] = nums[i - 1] * 2
+                nums[i] = 0
+
+        new_nums = []
+    
+        for num in nums:
+            if num != 0:
+                new_nums.append(num)
+    
+        for num in nums:
+            if num == 0:
+                new_nums.append(num)
+    
+        return new_nums
