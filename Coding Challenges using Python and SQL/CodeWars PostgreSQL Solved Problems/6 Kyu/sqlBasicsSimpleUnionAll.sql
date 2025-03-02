b2647b05a15f8e7ba6618e64ccbbeba5703633f5
christianebacani <christianbacani581@gmail.com>
2025-03-02 12:06:05 +0800

    Added new sub-directory and postgresql file

---
+++ Coding Challenges using Python and SQL/CodeWars PostgreSQL Solved Problems/6 Kyu/sqlBasicsSimpleUnionAll.sql
@@ -0,0 +1,31 @@
+-- 6 Kyu
+-- SQL Basics: Simple UNION ALL
+
+SELECT
+  'US' AS location,
+  id,
+  name,
+  price,
+  card_name,
+  card_number,
+  transaction_date
+FROM
+  ussales
+WHERE
+  price > 50.00
+UNION ALL
+SELECT
+  'EU' AS location,
+  id,
+  name,
+  price,
+  card_name,
+  card_number,
+  transaction_date
+FROM
+  eusales
+WHERE
+  price > 50.00
+ORDER BY
+  location DESC,
+  id;
