08f217b1ae9f743d59e3f0dddf16431a1aa7f6e9
Christiane Rhely Joselle Bacani <121742592+christianebacani@users.noreply.github.com>
2025-04-05 14:56:04 +0800

    Update content

--- Data Warehousing Concepts/Data Cleaning Processes using PostgreSQL/field_name_revisioning.sql
+++ Data Warehousing Concepts/Data Cleaning Processes using PostgreSQL/field_name_revisioning.sql
@@ -1,94 +1,94 @@
 /*
-	Field Name Revisioning using PostgreSQL
+    Field Name Revisioning using PostgreSQL
 */
 
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"Estimate ('000) 2018" TO
-	estimate_num_of_poor_fam_2018;
+    "Estimate ('000) 2018" TO
+    estimate_num_of_poor_fam_2018;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"Estimate ('000) 2021" TO
-	estimate_num_of_poor_fam_2021;
+    "Estimate ('000) 2021" TO
+    estimate_num_of_poor_fam_2021;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"Estimate ('000) 2023" TO
-	estimate_num_of_poor_fam_2023;
+    "Estimate ('000) 2023" TO
+    estimate_num_of_poor_fam_2023;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"Coefficient of Variation 2018" TO
-	coefficient_of_variation_2018;
+    "Coefficient of Variation 2018" TO
+    coefficient_of_variation_2018;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"Coefficient of Variation 2021" TO
-	coefficient_of_variation_2021;
+    "Coefficient of Variation 2021" TO
+    coefficient_of_variation_2021;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"Coefficient of Variation 2023" TO
-	coefficient_of_variation_2023;
+    "Coefficient of Variation 2023" TO
+    coefficient_of_variation_2023;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"Standard Error 2018" TO
-	standard_error_2018;
+    "Standard Error 2018" TO
+    standard_error_2018;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"Standard Error 2021" TO
-	standard_error_2021;
+    "Standard Error 2021" TO
+    standard_error_2021;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"Standard Error 2023" TO
-	standard_error_2023;
+    "Standard Error 2023" TO
+    standard_error_2023;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"95% Confidence Interval (Lower Limit) 2018" TO
-	confidence_interval_of_95_pct_lower_limit_at_2018;
+    "95% Confidence Interval (Lower Limit) 2018" TO
+    confidence_interval_of_95_pct_lower_limit_at_2018;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"95% Confidence Interval (Lower Limit) 2021" TO
-	confidence_interval_of_95_pct_lower_limit_at_2021;
+    "95% Confidence Interval (Lower Limit) 2021" TO
+    confidence_interval_of_95_pct_lower_limit_at_2021;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"95% Confidence Interval (Lower Limit) 2023" TO
-	confidence_interval_of_95_pct_lower_limit_at_2023;
+    "95% Confidence Interval (Lower Limit) 2023" TO
+    confidence_interval_of_95_pct_lower_limit_at_2023;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"95% Confidence Interval (Upper Limit) 2018" TO
-	confidence_interval_of_95_pct_upper_limit_at_2018;
+    "95% Confidence Interval (Upper Limit) 2018" TO
+    confidence_interval_of_95_pct_upper_limit_at_2018;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"95% Confidence Interval (Upper Limit) 2021" TO
-	confidence_interval_of_95_pct_upper_limit_at_2021;
+    "95% Confidence Interval (Upper Limit) 2021" TO
+    confidence_interval_of_95_pct_upper_limit_at_2021;
 
 ALTER TABLE
-	magnitudes
+    magnitudes
 RENAME COLUMN
-	"95% Confidence Interval (Upper Limit) 2023" TO
-	confidence_interval_of_95_pct_upper_limit_at_2023;
+    "95% Confidence Interval (Upper Limit) 2023" TO
+    confidence_interval_of_95_pct_upper_limit_at_2023;
