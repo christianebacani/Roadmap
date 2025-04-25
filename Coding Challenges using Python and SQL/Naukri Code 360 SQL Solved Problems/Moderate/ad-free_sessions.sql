17136bc0774d77af327574ba4e6cab8bfa4a487a
christianebacani <christianbacani581@gmail.com>
2025-04-25 13:02:26 +0800

    Added new sql file

---
+++ Coding Challenges using Python and SQL/Naukri Code 360 SQL Solved Problems/Moderate/ad-free_sessions.sql
@@ -0,0 +1,22 @@
+-- Question: Ad-Free Sessions
+-- Categories: Moderate
+
+WITH
+    session_and_num_ads AS (
+SELECT
+    session_id,
+    customer_id,
+    start_time,
+    end_time,
+    (SELECT COUNT(ad_id) FROM Ads WHERE Ads.customer_id = Playback.customer_id AND Ads.timestamp BETWEEN Playback.start_time AND Playback.end_time) AS number_of_ads_appeared
+FROM
+    Playback)
+
+
+SELECT
+    session_id
+FROM
+    session_and_num_ads
+WHERE
+    number_of_ads_appeared  = 0;
+
