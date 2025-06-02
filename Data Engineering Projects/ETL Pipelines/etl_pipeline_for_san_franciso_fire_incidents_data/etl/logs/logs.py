d983b0d0661d47baabe3d1307f0d18e6a89d56c8
christianebacani <christianbacani581@gmail.com>
2025-06-02 17:17:04 +0800

    Added new content to execute different extract/stage and transformation phase

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/logs/logs.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/logs/logs.py
@@ -1,7 +1,12 @@
 '''
     Data Pipeline Logs Module
 '''
+import sys
+sys.path.append('etl')
 from datetime import datetime
+from ingest.ingest import ingest_raw_data
+from extract.extract import extract_ingested_data
+from transform.data_processing import processing_extracted_data
 
 def log_messages(message: str) -> None:
     '''
@@ -18,5 +23,20 @@ def log_messages(message: str) -> None:
 
 # Execution of the Pipeline
 log_messages('Initiating ETL Pipeline for San Francisco Fire Incidents Data')
+
+# Ingestion Phase
+log_messages('Initiating Ingestion Phase')
+ingest_raw_data("https://data.sfgov.org/resource/wr8u-xric.csv", 705908)
+log_messages('Ingestion Phase Ended')
+
+# Extraction/Staging Phase
+log_messages('Initiating Extraction/Staging Phase')
+extract_ingested_data('data/raw/san_francisco_fire_incidents_data')
+log_messages('Extraction/Staging Phase')
+
+# Transformation Phase
+log_messages('Initiating Transformation Phase')
+processing_extracted_data('data/stage/san_francisco_fire_incidents_data')
+log_messages('Transformation Phase Ended')
 
 log_messages('ETL Pipeline for San Francisco Fire Incidents Data Ended')
