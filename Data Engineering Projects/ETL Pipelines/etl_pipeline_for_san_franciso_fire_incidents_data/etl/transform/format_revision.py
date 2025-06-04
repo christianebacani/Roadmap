0d27d4aff3f7bb77514eea75110294007d1d9ba5
christianebacani <christianbacani581@gmail.com>
2025-06-04 17:26:23 +0800

    Modified the content to focus on the column name of the data that needs to revise the format

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/format_revision.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/format_revision.py
@@ -15,12 +15,53 @@ def revise_data_format(dataframe: pd.DataFrame) -> pd.DataFrame:
     for column in columns:
         data[column] = []
     
+    # Format revisioning for making sure the values are consistent to minimize deviation
     for _, row in dataframe.iterrows():
         for column in columns:
             value = row.get(column)
 
-            if value is None:
-                data[column].append(value)
-                continue
+            if column == 'address':
+                street_type_abbreviations = {
+                    'St.': 'Street',
+                    'Rd.': 'Road',
+                    'Av.': 'Avenue',
+                    'Blvd.': 'Boulevard',
+                    'Dr.': 'Drive',
+                    'Ln.': 'Lane',
+                    'Wy.': 'Way,',
+                    'Ct.': 'Court',
+                    'Pl.': 'Place',
+                    'Cir.': 'Circle',
+                    'Hy.': 'Highway',
+                    'Tpke.': 'Turnpike',
+                    'Expy.': 'Expressway',
+                    'Sq.': 'Square',
+                    'Bl.': 'Block'
+                }
+                for abbreviation, street_type in street_type_abbreviations.items():
+                    value = str(value).replace(abbreviation, street_type)
+                
+                
+            elif column == 'city':
+                abbreviations = {
+                    'SFO': 'San Francisco International Airport area',
+                    'SF': 'San Francisco'
+                }
 
-            # TODO: Implement a functionality to format values
+                for abbreviation, name in abbreviations.items():
+                    value = str(value).replace(abbreviation, name)
+
+            elif column == 'point':
+                value = str(value).replace('POINT (', '')
+                value = str(value).replace(')', '')
+                value = ', '.join(value.split())
+            
+            elif isinstance(value, str) and '-' in value:
+                value = str(value).replace('-', ' - ')
+            
+            else:
+                pass
+
+            data[column].append(value)
+    
+    return pd.DataFrame(data)
