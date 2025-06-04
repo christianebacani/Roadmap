9489b0f13f857ac5937f702918d74ba0eff655f4
christianebacani <christianbacani581@gmail.com>
2025-06-04 17:25:47 +0800

    Modified the process of transforming the data

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_processing.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_processing.py
@@ -6,6 +6,7 @@ from glob import glob
 import pandas as pd
 from transform.data_imputation import impute_data
 from transform.type_casting import cast_datatype
+from transform.format_revision import revise_data_format
 
 def processing_extracted_data(subdirectory_path: str) -> None:
     '''
@@ -27,6 +28,13 @@ def processing_extracted_data(subdirectory_path: str) -> None:
     # Processing extracted/staged datasets for different transformation phases
     for dataset_number in range(1, total_datasets + 1):
         filepath = f'{subdirectory_path}/{dataset_name}({dataset_number}).csv'
+
         dataframe = pd.read_csv(filepath)
-        dataframe = cast_datatype(impute_data(dataframe))
-        
+        imputed_dataframe = impute_data(dataframe)
+        type_casted_dataframe = cast_datatype(imputed_dataframe)
+        revised_dataframe = revise_data_format(type_casted_dataframe)
+        transformed_dataframe = revised_dataframe
+
+        target_filepath = f'{target_subdirectory_path}/{dataset_name}({dataset_number}).csv'
+        transformed_dataframe.to_csv(target_filepath, index=False)
+        break
