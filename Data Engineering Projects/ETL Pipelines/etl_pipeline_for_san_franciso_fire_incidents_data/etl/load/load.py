5b1a2b44f8b05598c49fdef86d84b4cd7cb8738c
christianebacani <christianbacani581@gmail.com>
2025-06-05 19:55:11 +0800

    Initialize SQLAlchemy Engine for communicating to the PostgreSQL Database

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/load/load.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/load/load.py
@@ -2,12 +2,21 @@
     Data Loading Module
 '''
 from sqlalchemy import create_engine
+from load.data_integration import integrate_dataset
 
 def load_to_postgresql_db(subdirectory_path: str) -> None:
     '''
         Data Loading Function
     '''
-    username = '<YOUR_USERNAME>'
-    password = '<YOUR_PASSWORD>'
-    port = '<YOUR_PORT_NUMBER>'
-    database_name = 'san_francisco_fire_incidents_data'
+    # PostgreSQL Database Credentials
+    username = '<USERNAME>'
+    password = '<PASSWORD>'
+    hostname = '<HOSTNAME>'
+    port = '<PORT_NUMBER_OF_YOUR_POSTGRESQL_DB>'
+    database = 'san_francisco_fire_incidents_db'
+
+    # Initializing SQLAlchemy Engine
+    engine = create_engine(f'postgresql://{username}:{password}@{hostname}:{port}/{database}')
+
+    # Integrate datasets for schema manipulation process later on
+    integrate_dataset(subdirectory_path)
