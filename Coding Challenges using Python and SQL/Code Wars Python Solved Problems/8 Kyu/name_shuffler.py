f7057a7b7eb2c917929a13773e3714443e191d64
christianebacani <christianbacani581@gmail.com>
2025-06-07 16:24:34 +0800

    Modified content of README.md

--- Coding Challenges using Python and SQL/Code Wars Python Solved Problems/8 Kyu/name_shuffler.py
+++
@@ -1,8 +0,0 @@
-# Question: Name Shuffler
-# Categories: 8 Kyu
-
-def name_shuffler(str_: str) -> str:
-    first_name = str_.split()[0]
-    last_name = str_.split()[1]
-
-    return last_name + ' ' + first_name
--- Coding Challenges using Python and SQL/CodeForces Pyton Solved Problems/A Categories/281A_word_capitalization.py
+++
@@ -1,5 +0,0 @@
-# 281A - Word Capitalization
-
-word = input().strip()
-word = word[0].capitalize() + word[1:]
-print(word)
