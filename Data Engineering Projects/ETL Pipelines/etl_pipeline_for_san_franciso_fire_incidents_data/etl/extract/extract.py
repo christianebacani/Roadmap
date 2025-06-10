f0918a163926290de5c0fd3ad4a345b153404828
christianebacani <christianbacani581@gmail.com>
2025-06-10 21:00:06 +0800

    Modified the content to extract by staging/integrating ingested datasets before transformation phase

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/extract/extract.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/extract/extract.py
@@ -16,18 +16,19 @@ def extract_ingested_data(subdirectory_path: str) -> None:
     if not os.path.exists(target_subdirectory_path):
         os.makedirs(target_subdirectory_path)
     
-    # We can check the data if it's already ingested, return None if yes
+    # We can check the data if it's already extracted and integrated/staged, return None if yes
     if len(glob(f'{target_subdirectory_path}/*.csv')) > 0:
         return
-    
-    dataset_name = 'san_francisco_fire_incidents_data'
+
+    integrated_dataframe = pd.read_csv(f'{subdirectory_path}/san_francisco_fire_incidents_data(1).csv')
     total_datasets = len(glob(f'{subdirectory_path}/*.csv'))
 
-    # Extract/Ingest data to be ready for transformation phase using pandas library
-    for dataset_number in range(1, total_datasets + 1):
-        filepath = f'{subdirectory_path}/{dataset_name}({dataset_number}).csv'
-        ingested_dataframe = pd.read_csv(filepath, encoding='cp1252')
-        
-        target_filepath = f'{target_subdirectory_path}/{dataset_name}({dataset_number}).csv'
-        ingested_dataframe.to_csv(target_filepath, index=False)
-        print(f'Successfully extracted/staged {dataset_name}({dataset_number}) dataset')
+    # Extract ingested datasets and integrate/stage it before transformation phase using pandas library and concat method
+    for dataset_number in range(2, total_datasets + 1):
+        ingested_dataframe = pd.read_csv(f'{subdirectory_path}/san_francisco_fire_incidents_data({dataset_number}).csv', encoding='cp1252')
+        integrated_dataframe = pd.concat([integrated_dataframe, ingested_dataframe], ignore_index=True)
+
+        print(f'Successfully integrated san_francisco_fire_incidents_data({dataset_number})')
+    
+    target_filepath = f'{target_subdirectory_path}/san_francisco_fire_incidents_integrated_data.csv'
+    integrated_dataframe.to_csv(target_filepath, index=False)
