6cd94f606e5dfdc17010caea88593b689b565bae
christianebacani <christianbacani581@gmail.com>
2025-06-10 21:03:30 +0800

    Modified the content of the module to get the imputed value of every variable

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_imputation.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_imputation.py
@@ -3,55 +3,26 @@
 '''
 import pandas as pd
 
-def impute_data(dataframe: pd.DataFrame) -> pd.DataFrame:
+def impute_values_from_the_staged_dataset(staged_dataframe: pd.Dataframe) -> None:
     '''
         Data Imputation Function
     '''
-    # Initializing a dictionary to store imputed values per column for data imputation process
-    columns = list(dataframe.keys())
-    column_and_imputed_value = {}
+    # Initializing a dictionary to store the imputed values per column name
+    columns = list(staged_dataframe.keys())
+    imputed_values = {}
 
+    # Data imputation process by using dataframe and group of row-based extraction to optimize the data parsing
     for column in columns:
-        column_and_imputed_value[column] = None
+        data = {column: []}
 
-    # Checking every data type to implement the imputations according to the datatype (Integer = median, Datetime and string = mode)
-    for column in columns:
-        values = []
-
-        for value in dataframe[column]:
-            if str(value).lower() != 'nan':
-                values.append(value)
+        for row_number in range(0, 705908, 1000):
+            data[column].extend(list(staged_dataframe[column][row_number : row_number + 1000]))
         
-        if values == []:
-            continue
-        
-        value_is_digit = True
+        # Checking if the values is a pure numerical value
+        try:
+            _ = pd.Series(data[column]).mean()
 
-        for value in values:
-            if not str(value).isdigit():
-                value_is_digit = False
-                break
-        
-        if value_is_digit:
-            column_and_imputed_value[column] = int(pd.Series(values).median())
-
-        else:
-            column_and_imputed_value[column] = pd.Series(values).mode()[0]
-    
-    # Initializing dictionary to store dataframe after imputation process
-    data = {}
-
-    for column in columns:
-        data[column] = []
-    
-    # Data Imputation
-    for _, row in dataframe.iterrows():
-        for column in columns:
-            value = row.get(column)
-
-            if str(value).lower() == 'nan':
-                value = column_and_imputed_value[column]
-
-            data[column].append(value)
-    
-    return pd.DataFrame(data)
+            
+        # Getting the mode for the impute value because the value is not numeric
+        except TypeError:
+            impute_value = pd.Series(data[column].mode()[0])
