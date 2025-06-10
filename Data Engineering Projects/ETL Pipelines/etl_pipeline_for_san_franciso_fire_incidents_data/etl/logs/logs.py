d8cb86de9616b4e58427b3009c8c04743e148eb3
christianebacani <christianbacani581@gmail.com>
2025-06-10 21:02:07 +0800

    Refactored the logs module

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/logs/logs.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/logs/logs.py
@@ -6,9 +6,6 @@ sys.path.append('etl')
 from datetime import datetime
 from ingest.ingest import ingest_raw_data
 from extract.extract import extract_ingested_data
-from transform.data_processing import processing_extracted_data
-from data_integration.data_integration import integrate_processed_datasets
-from data_schema_enforcement.data_schema_enforcement import enforce_data_schema
 
 def log_messages(message: str) -> None:
     '''
@@ -31,24 +28,9 @@ log_messages('Initiating Ingestion Phase')
 ingest_raw_data("https://data.sfgov.org/resource/wr8u-xric.csv", 705908)
 log_messages('Ingestion Phase Ended')
 
-# Extraction/Staging Phase
-log_messages('Initiating Extraction/Staging Phase')
+# Extraction Phase
+log_messages('Initiating Extraction Phase')
 extract_ingested_data('data/raw/san_francisco_fire_incidents_data')
-log_messages('Extraction/Staging Phase Ended')
-
-# Transformation Phase
-log_messages('Initiating Transformation Phase')
-processing_extracted_data('data/stage/san_francisco_fire_incidents_data')
-log_messages('Transformation Phase Ended')
-
-# Data Integration Phase
-log_messages('Initiating Data Integration Phase')
-integrate_processed_datasets()
-log_messages('Data Integration Phase Ended')
-
-# Data Schema Enforcement Phase
-log_messages('Initiating Data Schema Enforcement Phase')
-enforce_data_schema()
-log_messages('Data Schema Enforcement Phase Ended')
+log_messages('Extraction Phase Ended')
 
 log_messages('ETL Pipeline for San Francisco Fire Incidents Data Ended')
