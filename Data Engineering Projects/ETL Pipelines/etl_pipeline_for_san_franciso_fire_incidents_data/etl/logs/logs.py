9feb4773a4c649bd1f6e889f8c1b4538b3d0516b
christianebacani <christianbacani581@gmail.com>
2025-06-11 21:43:04 +0800

    Added new functions to execute

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/logs/logs.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/logs/logs.py
@@ -3,9 +3,11 @@
 '''
 import sys
 sys.path.append('etl')
+import pandas as pd
 from datetime import datetime
 from ingest.ingest import ingest_raw_data
 from extract.extract import extract_ingested_data
+from transform.data_imputation import impute_values_from_the_staged_dataset
 
 def log_messages(message: str) -> None:
     '''
@@ -32,5 +34,10 @@ log_messages('Ingestion Phase Ended')
 log_messages('Initiating Extraction Phase')
 extract_ingested_data('data/raw/san_francisco_fire_incidents_data')
 log_messages('Extraction Phase Ended')
+
+# Transformation Phase
+log_messages('Initiating Transformation Phase')
+impute_values_from_the_staged_dataset(pd.read_csv('data/stage/san_francisco_fire_incidents_data/san_francisco_fire_incidents_integrated_data.csv', low_memory=False))
+log_messages('Transformation Phase Ended')
 
 log_messages('ETL Pipeline for San Francisco Fire Incidents Data Ended')
