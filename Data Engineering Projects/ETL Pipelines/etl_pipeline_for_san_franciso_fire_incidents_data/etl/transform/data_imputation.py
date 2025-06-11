4523fc744c2edcce63d16f15158b5d4404eac43f
christianebacani <christianbacani581@gmail.com>
2025-06-11 21:45:15 +0800

    Added new function, Added new functionalities to get the all the values per column and storing it in a csv file

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_imputation.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_imputation.py
@@ -3,26 +3,53 @@
 '''
 import pandas as pd
 
-def impute_values_from_the_staged_dataset(staged_dataframe: pd.Dataframe) -> None:
+def return_the_list_of_columns_that_impute_mean_values() -> str:
+    '''
+        List of columns that impute mean values Function
+    '''
+    # TODO : Get the columns that impute mean values
+    return '''suppression_units
+    suppression_personnel
+    ems_units
+    ems_personnel
+    other_units
+    other_personnel
+    estimated_property_loss
+    estimated_contents_loss
+    fire_fatalities
+    fire_injuries
+    civilian_fatalities
+    civilian_injuries
+    number_of_alarms
+    number_of_floors_with_minimum_damage
+    number_of_floors_with_significant_damage
+    number_of_floors_with_heavy_damage
+    number_of_floors_with_extreme_damage'''
+
+def impute_values_from_the_staged_dataset(staged_dataframe: pd.DataFrame) -> None:
     '''
         Data Imputation Function
     '''
-    # Initializing a dictionary to store the imputed values per column name
-    columns = list(staged_dataframe.keys())
-    imputed_values = {}
+    # Get the list of columns that impute mean values for numerical data
+    list_of_columns_that_impute_mean_values = return_the_list_of_columns_that_impute_mean_values().split('\n')
 
+    for i in range(len(list_of_columns_that_impute_mean_values)):
+        list_of_columns_that_impute_mean_values[i] = list_of_columns_that_impute_mean_values[i].strip()
+    
+    columns = list(staged_dataframe.keys())
+    
     # Data imputation process by using dataframe and group of row-based extraction to optimize the data parsing
     for column in columns:
         data = {column: []}
 
-        for row_number in range(0, 705908, 1000):
+        for row_number in range(0, 705909, 1000):
             data[column].extend(list(staged_dataframe[column][row_number : row_number + 1000]))
-        
-        # Checking if the values is a pure numerical value
-        try:
-            _ = pd.Series(data[column]).mean()
+            print(f'Extracting data of column: {column} from {row_number}-{row_number + 1000} rows for data imputation')
 
-            
-        # Getting the mode for the impute value because the value is not numeric
-        except TypeError:
-            impute_value = pd.Series(data[column].mode()[0])
+        # Convert to dataframe and storing dataframe to the staging directory path as a csv file (It consumes more processing time if we use mean(). median(), or mode() right away)
+        dataframe = pd.DataFrame(data)
+        target_filepath = f'data/stage/san_francisco_fire_incidents_data/{column}_data.csv'
+        dataframe.to_csv(target_filepath, index=False)
+    
+    # Initializing a dictionary to store the imputed values per column name
+    imputed_values = {}
