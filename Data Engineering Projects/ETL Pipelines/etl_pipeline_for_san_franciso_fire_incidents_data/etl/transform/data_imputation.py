8beb07df3765220766d880ab30d8a37d0dbad5cd
christianebacani <christianbacani581@gmail.com>
2025-06-12 21:23:30 +0800

    Added content to get the impute values by determining the data type of the variable and using scipy library to determine the skewness which could help for mean and median options of data imputation

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_imputation.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_imputation.py
@@ -2,54 +2,52 @@
     Data Imputation Module
 '''
 import pandas as pd
+from scipy.stats import skew
 
-def return_the_list_of_columns_that_impute_mean_values() -> str:
-    '''
-        List of columns that impute mean values Function
-    '''
-    # TODO : Get the columns that impute mean values
-    return '''suppression_units
-    suppression_personnel
-    ems_units
-    ems_personnel
-    other_units
-    other_personnel
-    estimated_property_loss
-    estimated_contents_loss
-    fire_fatalities
-    fire_injuries
-    civilian_fatalities
-    civilian_injuries
-    number_of_alarms
-    number_of_floors_with_minimum_damage
-    number_of_floors_with_significant_damage
-    number_of_floors_with_heavy_damage
-    number_of_floors_with_extreme_damage'''
-
-def impute_values_from_the_staged_dataset(staged_dataframe: pd.DataFrame) -> None:
+def impute_values_from_the_integrated_datasetr(dataframe: pd.DataFrame) -> pd.DataFrame:
     '''
         Data Imputation Function
     '''
-    # Get the list of columns that impute mean values for numerical data
-    list_of_columns_that_impute_mean_values = return_the_list_of_columns_that_impute_mean_values().split('\n')
+    # Using .keys() method to get all the variable/field names
+    columns = list(dataframe.keys())
+    
+    # Initializing a dictionary to store the variable/field name as a key and imputed value as a value
+    columns_and_imputed_value = {}
 
-    for i in range(len(list_of_columns_that_impute_mean_values)):
-        list_of_columns_that_impute_mean_values[i] = list_of_columns_that_impute_mean_values[i].strip()
-    
-    columns = list(staged_dataframe.keys())
-    
-    # Data imputation process by using dataframe and group of row-based extraction to optimize the data parsing
+    # Initializing a dictionary to store the variable/field names as a key and skewness as a value
+    columns_and_skewness = {}
+
     for column in columns:
-        data = {column: []}
+        values = []
 
         for row_number in range(0, 705909, 1000):
-            data[column].extend(list(staged_dataframe[column][row_number : row_number + 1000]))
-            print(f'Extracting data of column: {column} from {row_number}-{row_number + 1000} rows for data imputation')
+            values.extend(list(dataframe[column][row_number : row_number + 1000]))
+            print(f'Extracting the values of column: {column} from {row_number}-{row_number} rows')
 
-        # Convert to dataframe and storing dataframe to the staging directory path as a csv file (It consumes more processing time if we use mean(). median(), or mode() right away)
-        dataframe = pd.DataFrame(data)
-        target_filepath = f'data/stage/san_francisco_fire_incidents_data/{column}_data.csv'
-        dataframe.to_csv(target_filepath, index=False)
+        data = pd.Series(values)
+
+        # Check if it's a numerical value using try and catch statement
+        try:
+            data.median()
+            skewness = skew(data.dropna())
+            columns_and_skewness[column] = skewness
+
+        # Get the mode value if it's not a numerical value
+        except TypeError:
+            data = data.dropna()
+            columns_and_imputed_value[column] = data.mode()[0]
+
+    # Identifying the skewness of the numerical value to determine the imputed value
+    for key, value in columns_and_skewness.items():
+        if value == 0 or value < 0.5:
+            imputed_value = int(pd.Series(dataframe[key].dropna()).mean())
+        
+        else:
+            imputed_value = int(pd.Series(dataframe[key].dropna()).median())
+        
+        columns_and_imputed_value[key] = imputed_value
     
-    # Initializing a dictionary to store the imputed values per column name
-    imputed_values = {}
+    for column in columns:
+        dataframe[column] = dataframe[column].fillna(columns_and_imputed_value[column])
+    
+    return dataframe
