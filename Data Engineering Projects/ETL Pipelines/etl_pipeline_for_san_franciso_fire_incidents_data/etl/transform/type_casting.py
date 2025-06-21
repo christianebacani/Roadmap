662bb4d6eaac8fa07f95ecc46063e9e4a73b5474
christianebacani <christianbacani581@gmail.com>
2025-06-22 02:12:11 +0800

    MODIFY: Remove most of the functionalities inside the function

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/type_casting.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/type_casting.py
@@ -10,42 +10,5 @@ def cast_data_type(dataframe: pd.DataFrame) -> pd.DataFrame:
     '''
     # Using .keys() method to get all the variable/field names
     columns = list(dataframe.keys())
-    data = {}
 
-    for column in columns:
-        data[column] = []
-        
-    # Type casting process by using the name of the column or using try and catch statement
-    for _, row in dataframe.iterrows():
-        for column in columns:
-            value = row.get(column)
-
-            if column == 'incident_date':
-                value = datetime.strptime(str(value), '%Y-%m-%dT%H:%M:%S.%f').date()
-                data[column].append(value)
-                continue
-
-            elif column in ['data_as_of', 'data_loaded_at'] or 'dttm' in column:
-                value = datetime.strptime(str(value), '%Y-%m-%dT%H:%M:%S.%f')
-                data[column].append(value)
-                continue
-
-            else:
-                pass
-            
-            if str(value).isdigit():
-                value = int(value)
-                data[column].append(value)
-                continue
-
-            try:
-                value = float(value)
-            
-            except ValueError:
-                value = str(value)
-            
-            data[column].append(value)
-
-    type_casted_dataframe = pd.DataFrame(data)
-
-    return type_casted_dataframe
+    # TODO: Add more functions here...
