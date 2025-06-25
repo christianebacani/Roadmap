17ca9e1ffa249e23305d4917e7a606b44a0fc21b
christianebacani <christianbacani581@gmail.com>
2025-06-26 00:35:51 +0800

    MODIFY: Remove the credentials in the module to prevent data exploitations

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/load/load.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/load/load.py
@@ -10,11 +10,11 @@ def load_to_postgresql_db(subdirectory_path: str) -> None:
         Data Load Function
     '''
     # Initialize the PostgreSQL Database Server Credentials
-    username = '<YOUR_USERNAME>'
-    password = '<YOUR_PASSWORD>'
-    hostname = '<YOUR_HOSTNAME>'
-    port = '<YOUR_PORT_NUMBER>'
-    database = 'san_francisco_fire_incidents_db'
+    username = '<USERNAME>'
+    password = '<PASSWORD>'
+    hostname = '<HOSTNAME>'
+    port = '<PORT_NUMBER>'
+    database = 'san_francisco_fire_incidents_db'    
 
     # Initialize the query engine to map the dataframes to the database objects
     engine = create_engine(f'postgresql://{username}:{password}@{hostname}:{port}/{database}')
