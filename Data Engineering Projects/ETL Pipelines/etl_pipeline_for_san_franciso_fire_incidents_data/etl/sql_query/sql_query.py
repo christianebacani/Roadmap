9885ec2b93edc7dad55a4b978a144ae163c107b3
christianebacani <christianbacani581@gmail.com>
2025-06-26 00:35:07 +0800

    ADD: Added new package 'sql_query' for testing queries in the database object after loading phase

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/sql_query/sql_query.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/sql_query/sql_query.py
@@ -1,8 +1,22 @@
 '''
     SQL Query Module
 '''
-def query(query_command: str, table_name: str) -> None:
+from sqlalchemy import create_engine
+import pandas as pd
+
+def query(query_command: str) -> None:
     '''
         SQL Query Function
     '''
-    # TODO: Add more functionalities here...
+    # Initialize the PostgreSQL Database Server Credentials
+    username = '<USERNAME>'
+    password = '<PASSWORD>'
+    hostname = '<HOSTNAME>'
+    port = '<PORT_NUMBER>'
+    database = 'san_francisco_fire_incidents_db'
+
+    # Initialize the query engine to test sql queries
+    engine = create_engine(f'postgresql://{username}:{password}@{hostname}:{port}/{database}')
+
+    # SQL Query
+    print(pd.read_sql(query_command, engine))
