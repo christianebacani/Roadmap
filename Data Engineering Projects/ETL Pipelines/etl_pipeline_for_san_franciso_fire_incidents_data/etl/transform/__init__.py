16d5c96d5c539b8ccfbdc4afe5955e0a70591f84
christianebacani <christianbacani581@gmail.com>
2025-06-26 17:02:35 +0800

    ADD: Added new modules to initialize in the transform package

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/__init__.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/__init__.py
@@ -3,4 +3,7 @@ from . import data_imputation
 from . import data_partition
 from . import type_casting
 from . import format_revision
+from . import data_integration
 from . import data_deduplication
+from . import load_processed_dataset
+from . import remove_datasets
