b9ed8b9d301097fb73d82983af775883b903c1af
christianebacani <christianbacani581@gmail.com>
2025-06-26 17:01:17 +0800

    ADD: Added new functions to initialize from new package 'sql_query'

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/logs/logs.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/logs/logs.py
@@ -9,6 +9,8 @@ from ingest.ingest import ingest_raw_data
 from extract.extract import extract_ingested_data
 from transform.data_transformation import transform_staged_dataset
 from load.data_schema_revision import revise_schema
+from load.load import load_to_postgresql_db
+from sql_query.sql_query import query
 
 def log_messages(message: str) -> None:
     '''
@@ -44,6 +46,14 @@ log_messages('Transformation Phase Ended')
 # Loading Phase
 log_messages('Initiating Loading Phase')
 revise_schema(pd.read_csv('data/processed/san_francisco_fire_incidents_data/san_francisco_fire_incidents_processed_data.csv', low_memory=False))
+load_to_postgresql_db('data/processed/san_francisco_fire_incidents_data')
 log_messages('Loading Phase Ended')
+
+# SQL Query
+log_messages('Initiating SQL Query Phase')
+query('SELECT * FROM facts_fire_incidents_data')
+query('SELECT * FROM dim_incident_number')
+query('SELECT * FROM dim_id')
+log_messages('SQL Query Phase Ended')
 
 log_messages('ETL Pipeline for San Francisco Fire Incidents Data Ended')
