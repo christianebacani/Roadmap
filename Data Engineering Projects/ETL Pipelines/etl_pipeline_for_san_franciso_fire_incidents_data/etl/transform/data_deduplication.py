ecb5c82c0deedddb3fcd76027eaf1570fed6f8e0
christianebacani <christianbacani581@gmail.com>
2025-07-02 23:33:32 +0800

    MODIFY: Added comment

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_deduplication.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_deduplication.py
@@ -7,6 +7,7 @@ def deduplicate_integrated_dataset(dataframe: pd.DataFrame) -> pd.DataFrame:
     '''
         Data Deduplication Function
     '''
+    # Perform data deduplication process
     dataframe.drop_duplicates(subset=['incident_number', 'id'], inplace=True)
 
     return dataframe
