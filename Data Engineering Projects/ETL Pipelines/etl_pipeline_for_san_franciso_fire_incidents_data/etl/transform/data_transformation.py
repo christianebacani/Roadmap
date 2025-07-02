d3b59b09d07457c02922ecade878611633505622
christianebacani <christianbacani581@gmail.com>
2025-07-02 23:37:23 +0800

    MODIFY: Remove other functions and combine them inside the module to make it more readable and remove unnecessary modules from the transform package

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_transformation.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_transformation.py
@@ -1,17 +1,15 @@
 '''
     Data Transformation Module
 '''
-import pandas as pd
 import os
+import pandas as pd
 from transform.data_imputation import impute_values_from_the_integrated_dataset
+from transform.data_type_extraction import get_the_data_type_of_every_column
 from transform.data_partition import partition_integrated_staged_dataset
-from transform.type_casting import get_the_data_type_of_every_column
 from transform.type_casting import cast_data_type
 from transform.format_revision import revise_format
 from transform.data_integration import integrate_datasets
 from transform.data_deduplication import deduplicate_integrated_dataset
-from transform.remove_datasets import remove_partitioned_and_integrated_datasets
-from transform.load_processed_dataset import load_processed_integrated_dataset
 
 def transform_staged_dataset(staged_dataframe: pd.DataFrame) -> None:
     '''
@@ -41,11 +39,29 @@ def transform_staged_dataset(staged_dataframe: pd.DataFrame) -> None:
     # Integrate partitioned dataset after type casting and format revision process
     integrated_dataframe = integrate_datasets('data/stage/san_francisco_fire_incidents_data')
 
-    # Data deduplication
-    integrated_dataframe = deduplicate_integrated_dataset(integrated_dataframe)
-    
-    # Load the integrated processed dataset to the processed directory path for loading phase
-    load_processed_integrated_dataset(integrated_dataframe)
+    # Perform data deduplication to the integrated dataset
+    deduplicated_dataframe = deduplicate_integrated_dataset(integrated_dataframe)
 
-    # Remove partitioned and integrated dataset after transformation phase
-    remove_partitioned_and_integrated_datasets('data/stage/san_francisco_fire_incidents_data')    
+    # Load transform dataset to processed directory path
+    target_subdirectory_path = 'data/processed/san_francisco_fire_incidents_data'
+
+    if not os.path.exists(target_subdirectory_path):
+        os.makedirs(target_subdirectory_path)
+    
+    target_filepath = f'{target_subdirectory_path}/san_francisco_fire_incidents_processed_data.csv'
+    deduplicated_dataframe.to_csv(target_filepath, index=False)
+    print(f'Successfully load san_francisco_fire_incidents_data_processed_data.csv to processed directory path after transformation process')
+
+    # Remove unnecessary datasets and subdirectory path after transformation phase
+    if os.path.exists('data/stage/san_francisco_fire_incidents_data/san_francisco_fire_incidents_integrated_data.csv'):
+        os.remove('data/stage/san_francisco_fire_incidents_data/san_francisco_fire_incidents_integrated_data.csv')
+        print(f'Successfully remove san_francisco_fire_incidents_integrateed_data.csv after transformation phase')
+
+    for dataset_number in range(1, 706 + 1):
+        filepath = f'data/stage/san_francisco_fire_incidents_data/san_francisco_fire_incidents_data({dataset_number}).csv'
+        os.remove(filepath)
+        print(f'Successfully remove san_francisco_fire_incidents_data({dataset_name}).csv partitioned dataset after transformation phase')
+    
+    if os.path.exists('data/stage/san_francisco_fire_incidents_data'):
+        os.rmdir('data/stage/san_francisco_fire_incidents_data')
+        print(f'Successfully remove \'stage\' subdirectory path after transformation phase')
