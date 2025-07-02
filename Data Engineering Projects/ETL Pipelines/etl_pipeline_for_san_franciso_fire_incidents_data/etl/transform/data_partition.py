7f7b5189633e8cb5f5d994eaa0b5621969c1bc37
christianebacani <christianbacani581@gmail.com>
2025-07-02 23:34:07 +0800

    MODIFY: Added comment

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_partition.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_franciso_fire_incidents_data/etl/transform/data_partition.py
@@ -7,6 +7,7 @@ def partition_integrated_staged_dataset(dataframe: pd.DataFrame) -> None:
     '''
         Data Partition Function
     '''
+    # Perform data partitioning process
     for dataset_number, row_number in enumerate(range(0, 705908 + 1, 1000)):
         dataset_number += 1
         target_filepath = f'data/stage/san_francisco_fire_incidents_data/san_francisco_fire_incidentS_data({dataset_number}).csv'
