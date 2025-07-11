d0034d9d16829d74957dc3379d6ed5f0ba3aa79b
christianebacani <christianbacani581@gmail.com>
2025-07-12 00:50:17 +0800

    MODIFY: Added more functionalities to the module like getting the distinct values of every column

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/data_imputation.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/data_imputation.py
@@ -7,3 +7,29 @@ def impute_missing_values(integrated_dataframe: pd.DataFrame) -> pd.DataFrame:
     '''
         Data Imputation Function
     '''
+    # Initialized the columns of the integrated dataset
+    columns = list(integrated_dataframe.keys())
+    
+    # Get the distinct values of every column
+    column_and_distinct_values = {}
+
+    for column in columns:
+        column_and_distinct_values[column] = []
+    
+    for column in columns:
+        distinct_values = list(set(integrated_dataframe[column]))
+        column_and_distinct_values[column] = distinct_values
+    
+    # Display the column and distinct values for debugging purposes only
+    for column, distinct_values in column_and_distinct_values.items():
+        print(column)
+        print(f'{distinct_values}')
+        print()
+    
+    data_type_and_frequencies = {
+        'nan': 0,
+        'datetime': 0,
+        'string': 0,
+        'integer': 0,
+        'float': 0
+    }
