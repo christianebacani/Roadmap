38cf864923b7bcc8314bd2342ae6c56d36dd0677
christianebacani <christianbacani581@gmail.com>
2025-07-11 15:55:04 +0800

    ADD: Added logs.py module inside logs package to automate the process of implementing logs for different phases of data pipeline

---
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/logs/logs.py
@@ -0,0 +1,33 @@
+'''
+    Log Module
+'''
+import sys
+import os
+sys.path.insert(0, os.path.abspath('etl'))
+from datetime import datetime
+from ingest.ingest import ingest_raw_data
+from extract.extract import extract_ingested_datasets
+
+def log_progress(message: str) -> None:
+    '''
+        Log function
+    '''
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    with open('etl/logs/logs.txt', 'a') as f:
+        f.write(f'[{timestamp}]: {message}\n')
+        
+    f.close()
+
+if __name__ == '__main__':
+    log_progress('Initiating ETL Pipeline for San Francisco Budget Data')
+    
+    # Ingestion Phase
+    log_progress('Initiating Ingestion Phase')
+    ingest_raw_data('https://data.sfgov.org/resource/xdgd-c79v.csv', 360531)
+    log_progress('Ingestion Phase Ended')
+
+    # Extraction Phase
+    log_progress('Initiating Extraction Phase')
+    extract_ingested_datasets('data/raw/san_francisco_budget_data')
+    log_progress('Extraction Phase Ended')
