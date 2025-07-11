c1babd3eacf3bc9f95376a5a77965c1432ad3e5b
christianebacani <christianbacani581@gmail.com>
2025-07-11 21:43:11 +0800

    ADD: Added new module inside the transform package

---
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/data_imputation.py
@@ -0,0 +1,9 @@
+'''
+    Data Imputation Module
+'''
+import pandas as pd
+
+def impute_missing_values(integrated_dataframe: pd.DataFrame) -> pd.DataFrame:
+    '''
+        Data Imputation Function
+    '''
