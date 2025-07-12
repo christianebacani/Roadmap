2e81682bcbc4f76d6016941b0a975d79b32699e7
christianebacani <christianbacani581@gmail.com>
2025-07-12 15:04:25 +0800

    MODIFY: Modified the content on how to get the total number of staged datasets (excluding the integrated dataset)

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/transform.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/transform.py
@@ -10,25 +10,17 @@ def transform_extracted_datasets(subdirectory_path: str) -> None:
     '''
         Transform Function
     '''
-    # Create the target subdirectory path if it's not existed before data ingestion phase
-    target_subdirectory_path = f'data/processed/san_francisco_budget_data'
+    # Get the total number of staged datasets
+    total_number_of_datasets = 0
 
-    if not os.path.exists(target_subdirectory_path):
-        os.makedirs(target_subdirectory_path)
+    for csv_file in glob(f'{subdirectory_path}/*.csv'):
+        base_filename = str(csv_file).replace('data/staged/san_francisco_budget_data\\', '')
+        base_filename = base_filename.replace('.csv', '')
 
-    # Perform data integration before data imputation phase
-    total_number_of_datasets = len(glob(f'{subdirectory_path}/*.csv'))
-    integrated_dataframe = pd.read_csv(f'data/staged/san_francisco_budget_data/san_francisco_budget_data(1).csv')
-
-    for dataset_number in range(2, total_number_of_datasets + 1):
-        filepath = f'data/staged/san_francisco_budget_data/san_francisco_budget_data({dataset_number}).csv'
-        integrated_dataframe = pd.concat([integrated_dataframe, pd.read_csv(filepath)], ignore_index=True)
-        
-        print(f'Successfully integrate san_francisco_budget_data({dataset_number}).csv to the integrated dataset')
+        if base_filename != 'san_francisco_integrated_budget_data':
+            total_number_of_datasets += 1
     
-    target_filepath = f'{subdirectory_path}/san_francisco_integrated_budget_data.csv'
-    integrated_dataframe.to_csv(target_filepath, index=False)
-    print(f'Successfully perform data integration')
+    # TODO: Implement a function here from other module for performing data integration phase
 
     # Remove staged datasets after data integration phase
     for dataset_number in range(1, total_number_of_datasets + 1):
@@ -39,4 +31,8 @@ def transform_extracted_datasets(subdirectory_path: str) -> None:
             print(f'Successfully removed san_francisco_budget_data({dataset_number}).csv staged dataset')
 
     # Perform data imputation phase
-    impute_missing_values(pd.read_csv(f'{subdirectory_path}/san_francisco_integrated_budget_data.csv'))    
+    target_filepath = f'data/staged/san_francisco_budget_data/san_francisco_integrated_budget_data.csv'
+    integrated_dataframe = impute_missing_values(pd.read_csv(f'{subdirectory_path}/san_francisco_integrated_budget_data.csv', low_memory=False))
+    integrated_dataframe.to_csv(target_filepath, index=False)
+    print(f'Successfully perform data imputation')
+
