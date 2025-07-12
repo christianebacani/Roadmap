a79ea22eccb36d684fc18b717dad8f3b3d940cec
christianebacani <christianbacani581@gmail.com>
2025-07-12 22:38:19 +0800

    ADD: Added new module data_partition.py to perform data partitioning before other transformation occurs

---
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/data_partition.py
@@ -0,0 +1,20 @@
+'''
+    Data Partition Module
+'''
+import pandas as pd
+
+def partition(integrated_dataframe: pd.DataFrame) -> None:
+    '''
+        Data Partition Function
+    '''
+    # Perform data partition
+    target_subdirectory_path = 'data/staged/san_francisco_budget_data'
+
+    for dataset_number, row_number in enumerate(range(0, 360531 + 1, 1000)):
+        dataset_number += 1
+
+        target_filepath = f'{target_subdirectory_path}/san_francisco_budget_data({dataset_number}).csv'
+        partitioned_dataframe = integrated_dataframe[row_number : row_number + 1000]
+        partitioned_dataframe.to_csv(target_filepath, index=False)
+
+        print(f'Successfully partitioned {row_number}-{row_number + 999} rows of data from san_francisco_integrated_budget_data.csv')
