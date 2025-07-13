4da4d43462c3a84e9e0d86fd5714e04e9fb2d7cb
christianebacani <christianbacani581@gmail.com>
2025-07-13 23:42:56 +0800

    MODIFY: Added more content to the functions of the datatype_cast.py module

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/datatype_cast.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/datatype_cast.py
@@ -1,7 +1,30 @@
 '''
     Data Type Cast Module
 '''
-def cast_datatype(subdirectory_path: str) -> None:
+import pandas as pd
+
+def cast_datatype(total_number_of_datasets: int) -> None:
     '''
         Data Type Cast Function
     '''
+    # Perform type casting
+    for dataset_number in range(1, total_number_of_datasets + 1):
+        filepath = f'data/staged/san_francisco_budget_data/san_francisco_budget_data({dataset_number}).csv'
+        partitioned_dataframe = pd.read_csv(filepath)
+        
+        # Initialize the dictionary that stores type casted values of every column
+        columns = list(partitioned_dataframe.keys())
+        data = {}
+
+        for column in columns:
+            data[column] = []
+
+        for _, row in partitioned_dataframe.iterrows():
+            for column in columns:
+                value = row.get(column)
+
+                if str(value).lower() == 'nan':
+                    data[column].append(pd.NA)
+                    continue
+
+                # TODO: Implement more functionalities here...
