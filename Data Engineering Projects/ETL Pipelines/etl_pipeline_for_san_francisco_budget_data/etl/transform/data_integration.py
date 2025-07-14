2058d55d2211cfda475ac5e2c79d23f08d7ccf03
christianebacani <christianbacani581@gmail.com>
2025-07-14 23:43:22 +0800

    MODIFY: Modify the content of the function

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/data_integration.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/data_integration.py
@@ -1,21 +1,24 @@
 '''
     Data Integration Module
 '''
+from glob import glob
 import pandas as pd
 
-def integrate_datasets(total_number_of_datasets: int) -> None:
+def integrate_datasets(directory_path: str) -> None:
     '''
         Data Integration Function
     '''
-    # Perform data integration 
-    integrated_dataframe = pd.read_csv('data/staged/san_francisco_budget_data/san_francisco_budget_data(1).csv')
+    total_number_of_datasets = len(glob(f'{directory_path}/*.csv'))
+
+    # Perform data integration
+    integrated_dataframe = pd.read_csv(f'{directory_path}/san_francisco_budget_data(1).csv')
 
     for dataset_number in range(2, total_number_of_datasets + 1):
-        filepath = f'data/staged/san_francisco_budget_data/san_francisco_budget_data({dataset_number}).csv'
-        integrated_dataframe = pd.concat([integrated_dataframe, pd.read_csv(filepath)], ignore_index=True)
-
-        print(f'Successfully integrate san_francisco_budget_data({dataset_number}).csv')
+        filepath = f'{directory_path}/san_francisco_budget_data({dataset_number}).csv'
+        integrated_dataframe = pd.concat([integrated_dataframe, pd.read_csv(filepath)], ignore_index=False)
+        
+        print(f'Successfully perform data integration to san_francisco_budget_data({dataset_number}).csv')
     
-    target_filepath = f'data/staged/san_francisco_budget_data/san_francisco_integrated_budget_data.csv'
+    target_filepath = f'{directory_path}/san_francisco_integrated_budget_data.csv'
     integrated_dataframe.to_csv(target_filepath, index=False)
     print(f'Successfully perform data integration')
