f2c7a7020ad592e646aace5e5a4214c2add19eee
christianebacani <christianbacani581@gmail.com>
2025-07-14 23:45:28 +0800

    MODIFY: Added more functionalities for type casting the dataset

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/datatype_cast.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/datatype_cast.py
@@ -2,11 +2,15 @@
     Data Type Cast Module
 '''
 import pandas as pd
+from datetime import datetime
+from glob import glob
 
-def cast_datatype(total_number_of_datasets: int) -> None:
+def cast_datatype() -> None:
     '''
         Data Type Cast Function
     '''
+    total_number_of_datasets = len(glob(f'data/staged/san_francisco_budget_data/*.csv'))
+
     # Perform type casting
     for dataset_number in range(1, total_number_of_datasets + 1):
         filepath = f'data/staged/san_francisco_budget_data/san_francisco_budget_data({dataset_number}).csv'
@@ -27,4 +31,31 @@ def cast_datatype(total_number_of_datasets: int) -> None:
                     data[column].append(pd.NA)
                     continue
 
-                # TODO: Implement more functionalities here...
+                try:
+                    value = datetime.strptime(str(value), '%Y-%m-%dT%H:%M:%S.%f')
+                    data[column].append(value)
+                    continue
+
+                except:
+                    pass
+
+                if str(value).isdigit():
+                    value = int(value)
+                    data[column].append(value)
+                    continue
+
+                try:
+                    value = float(value)
+                    data[column].append(value)
+                
+                except:
+                    value = str(value)
+                    data[column].append(value)
+        
+        target_filepath = filepath
+        datatype_casted_dataframe = pd.DataFrame(data)
+        datatype_casted_dataframe.to_csv(target_filepath, index=False)
+
+        print(f'Successfully perform datatype casting to san_francisco_budget_data({dataset_number}).csv dataset')
+    
+    print(f'Successfully perform datatype casting')
