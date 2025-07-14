a5bea344d812a423fa232ac011d065683c37c4a3
christianebacani <christianbacani581@gmail.com>
2025-07-14 23:45:01 +0800

    MODIFY: Modify the content of the function

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/dataset_deletion.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/dataset_deletion.py
@@ -1,16 +1,29 @@
 '''
     Dataset Deletion Module
 '''
+from glob import glob
 import os
 
-def remove_unnecessary_datasets(total_number_of_datasets: int) -> None:
+def remove_unnecessary_datasets(directory_path: str) -> None:
     '''
         Dataset Deletion Function
     '''
+    # Get the total number of datasets from the staged directory path (excluding the integrated dataset)
+    total_number_of_datasets = 0
+
+    for csv_file in glob(f'{directory_path}/*.csv'):
+        base_filename = str(csv_file).replace('data/staged/san_francisco_budget_data\\', '')
+        base_filename = base_filename.replace('.csv', '')
+        
+        if base_filename != 'san_francisco_integrated_budget_data':
+            total_number_of_datasets += 1
+
     # Perform dataset deletion
     for dataset_number in range(1, total_number_of_datasets + 1):
-        filepath = f'data/staged/san_francisco_budget_data/san_francisco_budget_data({dataset_number}).csv'
+        filepath = f'{directory_path}/san_francisco_budget_data({dataset_number}).csv'
 
         if os.path.exists(filepath):
             os.remove(filepath)
-            print(f'Successfully removed san_francisco_budget_data({dataset_number}).csv staged dataset')
+            print(f'Successfully removed san_francisco_budget_data({dataset_number}).csv')
+
+    print(f'Successfully deleted staged datasets after data integration')
