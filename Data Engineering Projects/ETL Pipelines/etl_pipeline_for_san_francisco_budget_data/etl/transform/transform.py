c8e244e5d320e98be43dd8335a7d9772774bcb89
christianebacani <christianbacani581@gmail.com>
2025-07-14 23:48:13 +0800

    MODIFY: Refactored the content of the functions

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/transform.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/transform.py
@@ -1,40 +1,38 @@
 '''
     Transform Module
 '''
-import os
 import pandas as pd
-from glob import glob
+import os
 from transform.data_integration import integrate_datasets
 from transform.dataset_deletion import remove_unnecessary_datasets
 from transform.data_imputation import impute_missing_values
 from transform.data_partition import partition
+from transform.datatype_cast import cast_datatype
+from transform.format_revision import revise_dataset_format
 
 def transform_extracted_datasets(subdirectory_path: str) -> None:
     '''
         Transform Function
     '''
-    # Get the total number of staged datasets
-    total_number_of_datasets = 0
-
-    for csv_file in glob(f'{subdirectory_path}/*.csv'):
-        base_filename = str(csv_file).replace('data/staged/san_francisco_budget_data\\', '')
-        base_filename = base_filename.replace('.csv', '')
-
-        if base_filename != 'san_francisco_integrated_budget_data':
-            total_number_of_datasets += 1
-    
     # Data integration phase
-    integrate_datasets(total_number_of_datasets)
-    
+    integrate_datasets(subdirectory_path)
+
     # Dataset deletion phase
-    remove_unnecessary_datasets(total_number_of_datasets)
+    remove_unnecessary_datasets(subdirectory_path)
 
     # Data imputation phase
-    target_filepath = f'data/staged/san_francisco_budget_data/san_francisco_integrated_budget_data.csv'
-    integrated_dataframe = impute_missing_values(pd.read_csv(f'{subdirectory_path}/san_francisco_integrated_budget_data.csv', low_memory=False))
-    integrated_dataframe.to_csv(target_filepath, index=False)
-    print(f'Successfully perform data imputation')
+    dataframe = pd.read_csv(f'{subdirectory_path}/san_francisco_integrated_budget_data.csv', low_memory=False)
+    impute_missing_values(dataframe)
 
     # Data partition phase
-    partition(pd.read_csv(f'{subdirectory_path}/san_francisco_integrated_budget_data.csv', low_memory=False))
-    print(f'Successfully perform data partitioning')
+    partition(dataframe)
+
+    # Remove integrated dataset from the directory
+    if os.path.exists('data/staged/san_francisco_budget_data/san_francisco_integrated_budget_data.csv'):
+        os.remove('data/staged/san_francisco_budget_data/san_francisco_integrated_budget_data.csv')
+    
+    # Dataype casting phase
+    cast_datatype()
+
+    # Format revisioning phase
+    revise_dataset_format()
