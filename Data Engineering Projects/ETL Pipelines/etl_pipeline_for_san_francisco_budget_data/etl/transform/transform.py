9fa7f0b7126d2780dea0b728f4a5fea04800b3ba
christianebacani <christianbacani581@gmail.com>
2025-07-20 18:09:34 +0800

    MODIFY: Added functionalities to the transform.py module

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/transform.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/transform/transform.py
@@ -48,4 +48,17 @@ def transform_extracted_datasets(subdirectory_path: str) -> None:
     sf_integrated_budget_dataset = pd.read_csv('data/staged/san_francisco_budget_data/san_francisco_integrated_budget_data.csv', low_memory=False)
     sf_integrated_budget_dataset = deduplicate_dataset(sf_integrated_budget_dataset)
 
-    # TODO: Implement more functionalities here...
+    # Initialize the directory path to store the transform dataset
+    target_subdirectory_path = f'data/processed/san_francisco_budget_data'
+
+    if not os.path.exists(target_subdirectory_path):
+        os.makedirs(target_subdirectory_path)
+
+    # Load transformed dataset to the processed directory
+    target_filepath = f'{target_subdirectory_path}/san_francisco_integrated_budget_data.csv'
+    sf_integrated_budget_dataset.to_csv(target_filepath, index=False)
+    print(f'Successfully perform transformation logic to san francisco budget datasets and load to the processed directory')
+
+    # Remove integrated dataset from the directory
+    if os.path.exists('data/staged/san_francisco_budget_data/san_francisco_integrated_budget_data.csv'):
+        os.remove('data/staged/san_francisco_budget_data/san_francisco_integrated_budget_data.csv')
