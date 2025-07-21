06594884825d57d6e0f88a49968765400ee327d7
christianebacani <christianbacani581@gmail.com>
2025-07-21 17:34:29 +0800

    MODIFY: Deleted functionalities to initialize the dimension data, initializing dimension data as a dataframe and initializing the structure of facts data

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/load/data_schema_revision.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/load/data_schema_revision.py
@@ -35,5 +35,108 @@ def revise_schema(transformed_dataframe: pd.DataFrame) -> None:
         os.remove('data/processed/san_francisco_budget_data/san_francisco_integrated_budget_data.csv')
 
     total_number_of_datasets = len(glob(f'data/processed/san_francisco_budget_data/*.csv'))
+    columns = list(pd.read_csv('data/processed/san_francisco_budget_data/san_francisco_budget_data(1).csv').keys())
 
-    print(total_number_of_datasets)
+    # Initialize the structure of the dimension data
+    dimension_data = []
+
+    for column in columns:
+        if column == 'budget':
+            continue
+
+        primary_key = f'{column}_id'
+        dimension_data.append({
+            primary_key: [],
+            column: []
+        })
+    
+    # Initialize the non-key attributes of all dimension data
+    for column in columns:
+        if column == 'budget':
+            continue
+
+        values = []
+
+        for dataset_number in range(1, total_number_of_datasets + 1):
+            filepath = f'data/processed/san_francisco_budget_data/san_francisco_budget_data({dataset_number}).csv'
+            partitioned_dataframe = pd.read_csv(filepath)
+            values.extend(list(partitioned_dataframe[column]))
+
+        distinct_values = []
+
+        for i in range(len(values)):
+            if str(values[i]).lower() == 'nan':
+                continue
+
+            if values[i] in distinct_values:
+                continue
+
+            distinct_values.append(values[i])
+
+        values = distinct_values
+        
+        for i in range(len(dimension_data)):
+            if column not in dimension_data[i]:
+                continue
+
+            dimension_data[i][column] = values
+
+        print(f'Successfully initializes non-key attributes of dim_{column} dimension data')
+    
+    # Initialize the key attributes of all dimension data
+    for column in columns:
+        if column == 'budget':
+            continue
+
+        for i in range(len(dimension_data)):
+            if column not in dimension_data[i]:
+                continue
+            
+            primary_key_values = []
+            total_number_of_values = len(dimension_data[i][column])
+
+            for primary_key_value in range(1, total_number_of_values + 1):
+                primary_key_values.append(primary_key_value)
+
+            primary_key = f'{column}_id'
+            dimension_data[i][primary_key] = primary_key_values
+        
+        print(f'Successfully initializes key attributes of dim_{column} dimension data')
+    
+    # Initialize all dimension data as a dataframe  
+    for column in columns:
+        if column == 'budget':
+            continue
+
+        for i in range(len(dimension_data)):
+            if column not in dimension_data[i]:
+                continue
+
+            target_filepath = f'data/processed/san_francisco_budget_data/dim_{column}.csv'
+            dimension_dataframe = pd.DataFrame(dimension_data[i])
+            dimension_dataframe.to_csv(target_filepath, index=False)
+
+            print(f'Successfully initialize dim_{column} dimension data as a dataframe')
+
+    # Initialize the structure of facts data    
+    facts_data = {}
+
+    for column in columns:
+        if column == 'budget':
+            continue
+
+        foreign_key = f'{column}_id'
+        facts_data[foreign_key] = []
+
+    for column in columns:
+        if column == 'budget':
+            facts_data['budget'] = []
+            break
+    
+    column_and_dimension_df = {}
+
+    for column in columns:
+        if column == 'budget':
+            continue
+
+        # TODO: Initialize the dimension column as a key and the corresponding dimension dataframe as a value for the dictionary 'column_and_dimension_df'
