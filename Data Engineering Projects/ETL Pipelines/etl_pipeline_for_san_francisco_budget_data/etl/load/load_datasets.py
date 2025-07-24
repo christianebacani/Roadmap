9d6e62e71e4d9f827910c1ea8aa3214ee5fb82fb
christianebacani <christianbacani581@gmail.com>
2025-07-24 17:47:50 +0800

    MODIFY: Remove parameter from create_database_objects() function, Added more functionalities to initialize the necessary DDLs

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/load/load_datasets.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/load/load_datasets.py
@@ -18,7 +18,7 @@ def init_snowflake_connection():
     )
     return conn
 
-def create_database_objects(conn):
+def create_database_objects():
     '''
         Create function to create
         necessary database objects
@@ -56,7 +56,57 @@ def create_database_objects(conn):
 
         table_name_with_list_of_columns_and_datatypes[table_name] = list_of_columns_and_datatypes
 
-    # TODO: Add functionality to initialize the primary keys, foreign keys, and establish a query for transaction for database object creation
+    # Initialize the primary keys for all dimension datasets
+    for table_name, list_of_columns_and_datatypes in table_name_with_list_of_columns_and_datatypes.items():
+        if 'fact' in table_name:
+            continue
+
+        for i in range(len(list_of_columns_and_datatypes)):
+            column = str(list_of_columns_and_datatypes[i]).split()[0]
+
+            if 'id' not in column:
+                continue
+
+            list_of_columns_and_datatypes[i] = f'{column} INTEGER PRIMARY KEY'
+
+    # Initialize the foreign keys in facts dataset
+    for table_name, list_of_columns_and_datatypes in table_name_with_list_of_columns_and_datatypes.items():
+        if 'dim' in table_name:
+            continue
+
+        list_of_foreign_keys = []
+
+        for i in range(len(list_of_columns_and_datatypes)):
+            column = str(list_of_columns_and_datatypes[i]).split()[0]
+
+            if 'id' not in column:
+                continue
+
+            base_table_name = column.replace('_id', '')
+            table_name_reference = f'dim_{base_table_name}'
+
+            foreign_key = column
+            primary_key = column
+
+            list_of_foreign_keys.append(f'FOREIGN KEY({foreign_key}) REFERENCES {table_name_reference}({primary_key})')
+
+        list_of_columns_and_datatypes.extend(list_of_foreign_keys)
+
+    table_name_with_columns_metadata = table_name_with_list_of_columns_and_datatypes
+    table_name_and_ddl = {}
+
+    # Finalizing the DDL for creating the necessary database objects
+    for table_name, columns_metadata in table_name_with_columns_metadata.items():
+        columns_metadata = '(' + ', '.join(columns_metadata) + ')'
+        table_name_and_ddl[table_name] = columns_metadata
+
+    # Display the table name and DDL for debugging purposes only    
+    for table_name, ddl in table_name_and_ddl.items():
+        print(f'{table_name}')
+        print(ddl)
+        print()
+
+    # TODO: Implement a functionality to create the necessary database objects using the initialized DDLs
 
     # Close the established snowflake connection
     conn.close()
@@ -66,3 +116,5 @@ def load_datasets_to_snowflake(subdirectory_path: str) -> None:
         Load function to load datasets
         to Snowflake Databases
     '''
+    # Create database objects if necessary
+    create_database_objects()
