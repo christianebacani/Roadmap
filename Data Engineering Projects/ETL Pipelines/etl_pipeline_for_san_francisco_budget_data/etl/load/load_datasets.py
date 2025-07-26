bf8ad0b6a9a8064970824b5271d7615862749482
christianebacani <christianbacani581@gmail.com>
2025-07-26 23:56:03 +0800

    MODIFY: Added functionalities to create_database_objects function and load_datasets_to_snowflake function

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/load/load_datasets.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/load/load_datasets.py
@@ -36,6 +36,46 @@ def create_database_objects(conn):
         'float64': 'NUMBER(11, 2)'
     }
 
+    # Initialize a dictionary to store table name with their corresponding columns and columns datatype for initializing DDL commands
+    table_name_with_list_of_colums_datatype = {}
+
+    # Get the data type and column constrainst of every colums for every table
+    for csv_file in glob(f'data/processed/san_francisco_budget_data/*.csv'):
+        base_filename = str(csv_file).replace('data/processed/san_francisco_budget_data\\', '')
+        table_name = str(base_filename).replace('.csv', '')
+
+        dataframe = pd.read_csv(csv_file)
+        list_of_columns = list(dataframe.columns)
+
+        list_of_columns_datatype = []
+
+        for column in list_of_columns:
+            if 'id' in column:
+                list_of_columns_datatype.append(f'{column} INTEGER')
+            
+            else:
+                datatype = str(dataframe[column].dtype)
+                list_of_columns_datatype.append(f'{column} {column_datatypes[datatype]}')
+
+        table_name_with_list_of_colums_datatype[table_name] = list_of_columns_datatype
+
+    # Initialize the primary key constraints of every dimension table
+    for table_name, list_of_columns_datatype in table_name_with_list_of_colums_datatype.items():
+        if 'facts' in table_name:
+            continue
+
+        for i in range(len(list_of_columns_datatype)):
+            column = str(list_of_columns_datatype[i]).split()[0]
+
+            if 'id' in column:
+                list_of_columns_datatype[i] = f'{column} INTEGER PRIMARY KEY'
+
+    # Display the table name and their corresponding columns datatype and primary key constraints
+    for table_name, list_of_columns_datatype in table_name_with_list_of_colums_datatype.items():
+        print(f'{table_name}:')
+        print(list_of_columns_datatype)
+        print()
+
     # Close the snowflake cursor connection
     cursor.close()
 
@@ -44,3 +84,6 @@ def load_datasets_to_snowflake(subdirectory_path: str) -> None:
         Load function to load datasets
         to Snowflake Databases
     '''
+    # Initialize a snowflake connection and create necessary database objects
+    conn = init_snowflake_connection()
+    create_database_objects(conn)
