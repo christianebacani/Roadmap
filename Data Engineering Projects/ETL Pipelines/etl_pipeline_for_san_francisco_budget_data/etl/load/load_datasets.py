d8eb91226230486476555d3944d2abe5ac2e8f1c
christianebacani <christianbacani581@gmail.com>
2025-07-26 18:16:20 +0800

    MODIFY: Remove the functionalities of create_database_objects function because the there's multiple changes in data schema revisioning phase

--- Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/load/load_datasets.py
+++ Data Engineering Projects/ETL Pipelines/etl_pipeline_for_san_francisco_budget_data/etl/load/load_datasets.py
@@ -14,7 +14,7 @@ def init_snowflake_connection():
     conn = snowflake.connector.connect(
         user='<SNOWFLAKE_USERNAME>',
         password='<SNOWFLAKE_PASSWORD>',
-        account='<SNOWFLAKE_ACCOUNT_IDENTIFIER_>',
+        account='<SNOWFLAKE_ACCOUNT_IDENTIFIER>',
         warehouse='san_francisco_budget_data_warehouse',
         database='san_francisco_budget_data',
         schema='san_francisco_budget_data_star_schema'
@@ -36,82 +36,7 @@ def create_database_objects(conn):
         'float64': 'NUMBER(11, 2)'
     }
 
-    # Initialize a dictionary to store the table name and their corresponding columns and data types
-    table_name_with_list_of_columns_and_datatypes = {}
-
-    for csv_file in glob(f'data/processed/san_francisco_budget_data/*.csv'):
-        base_filename = str(csv_file).replace('data/processed/san_francisco_budget_data\\', '')
-        table_name = str(base_filename).replace('.csv', '')
-
-        dataframe = pd.read_csv(csv_file)
-        list_of_columns = list(dataframe.columns)
-
-        list_of_columns_and_datatypes = []
-
-        for column in list_of_columns:
-            if 'id' in column:
-                list_of_columns_and_datatypes.append(f'{column} INTEGER')
-                continue
-            
-            datatype = str(dataframe[column].dtype)
-            list_of_columns_and_datatypes.append(f'{column} {column_datatypes[datatype]}')
-
-        table_name_with_list_of_columns_and_datatypes[table_name] = list_of_columns_and_datatypes
-
-    # Initialize the primary keys for all dimension datasets
-    for table_name, list_of_columns_and_datatypes in table_name_with_list_of_columns_and_datatypes.items():
-        if 'fact' in table_name:
-            continue
-
-        for i in range(len(list_of_columns_and_datatypes)):
-            column = str(list_of_columns_and_datatypes[i]).split()[0]
-
-            if 'id' not in column:
-                continue
-
-            list_of_columns_and_datatypes[i] = f'{column} INTEGER PRIMARY KEY'
-
-    # Initialize the foreign keys in facts dataset
-    for table_name, list_of_columns_and_datatypes in table_name_with_list_of_columns_and_datatypes.items():
-        if 'dim' in table_name:
-            continue
-
-        list_of_foreign_keys = []
-
-        for i in range(len(list_of_columns_and_datatypes)):
-            column = str(list_of_columns_and_datatypes[i]).split()[0]
-
-            if 'id' not in column:
-                continue
-
-            base_table_name = column.replace('_id', '')
-            table_name_reference = f'dim_{base_table_name}'
-
-            foreign_key = column
-            primary_key = column
-
-            list_of_foreign_keys.append(f'FOREIGN KEY({foreign_key}) REFERENCES {table_name_reference}({primary_key})')
-
-        list_of_columns_and_datatypes.extend(list_of_foreign_keys)
-
-    table_name_with_columns_metadata = table_name_with_list_of_columns_and_datatypes
-    table_name_and_ddl_command = {}
-
-    # Finalizing the DDL commands for creating the necessary database objects
-    for table_name, columns_metadata in table_name_with_columns_metadata.items():
-        ddl_command = '(' + ', '.join(columns_metadata) + ')'
-        table_name_and_ddl_command[table_name] = ddl_command
-
-    # Create the necessary database objects using snowflake cursor
-    for table_name, ddl_command in table_name_and_ddl_command.items():
-        try:
-            cursor.execute(f"""CREATE OR REPLACE TABLE {table_name}{ddl_command};""")
-            print(f'Successfully created table: {table_name}')
-
-        except Exception as error_message:
-            print(f'Error creating table: {table_name}, error message: {error_message}')
-
-    # Close the established snowflake connection and cursor    
+    # Close the snowflake cursor connection
     cursor.close()
 
 def load_datasets_to_snowflake(subdirectory_path: str) -> None:
@@ -119,5 +44,3 @@ def load_datasets_to_snowflake(subdirectory_path: str) -> None:
         Load function to load datasets
         to Snowflake Databases
     '''
-    # Create the necessary database objects using snowflake connection as a parameter
-    create_database_objects(init_snowflake_connection())
