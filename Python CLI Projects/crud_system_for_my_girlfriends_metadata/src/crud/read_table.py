f43b8b6b46f605decfb69a2669c1b1dc05098379
christianebacani <christianbacani581@gmail.com>
2025-08-06 17:35:19 +0800

    ADD: Added new module

---
+++ Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/crud/read_table.py
@@ -0,0 +1,77 @@
+'''
+    Read Table Module
+'''
+import os
+import pandas as pd
+from glob import glob
+from src.utils.utils import init_engine
+
+# TODO: Move this function to utils module of utils package inside src directory also
+def get_table_names() -> list[str]:
+    '''
+        Get function to extract all table names
+        from PostgreSQL Database
+    '''
+    list_of_table_names = []
+
+    for csv_file in glob(f'src/rica_metadatas/*.csv'):
+        table_name = str(csv_file).replace('src/rica_metadatas\\', '')
+        table_name = str(table_name).replace('.csv', '')
+
+        list_of_table_names.append(table_name)
+
+    return list_of_table_names
+
+def display_table(table_name: str) -> None:
+    '''
+        Display function to display
+        chosen table
+    '''
+    engine = init_engine()
+    # Query using SQL Alchemy Engine
+    dataframe = pd.read_sql(f'SELECT * FROM {table_name}', engine)
+
+    print(dataframe)
+    input('\n\t\tPress any key to exit: ')
+
+def read_table_main_page() -> None:
+    '''
+        CLI-Based Main Page for Reading Tables
+    '''
+    while True:
+        # Display header
+        header = 'Read Tables'
+        print(f'\t\t\t{header}\n')
+
+        # Display options
+        options = get_table_names()
+        for number, option in enumerate(options):
+            print(f'\t\t\t{number + 1}.) Read {option} table')
+        
+        print(f'\t\t\t{len(options) + 1}.) Exit')
+
+        try:
+            choice = int(input('\n\t\tEnter your choice here: ').strip())
+            
+            if choice < 1 or choice > (len(options) + 1):
+                os.system('cls')
+                print(f'\t\tInvalid choice! Please try again.')
+                input('\t\tPress any key to continue: ')
+                os.system('cls')
+                continue
+
+            if choice == (len(options) + 1):
+                os.system('cls')
+                break
+    
+            os.system('cls')
+            display_table(options[int(choice) - 1])
+            os.system('cls')
+            continue
+
+        except:
+            os.system('cls')
+            print(f'\t\tInvalid choice! Please try again.')
+            input('\t\tPress any key to continue: ')
+            os.system('cls')
+            continue
