169929ec722f67f09557fafa889bc6e8365ebf86
christianebacani <christianbacani581@gmail.com>
2025-08-19 23:16:46 +0800

    MODIFY: Refactored the whole system including utils.py module of utils package

--- Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/utils/utils.py
+++ Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/utils/utils.py
@@ -1,53 +1,14 @@
 '''
-    Utils Module
+    Utilities Module
 '''
-import psycopg2
-from glob import glob
-from sqlalchemy import create_engine
+import os
 
-def init_engine() -> object:
+def display_invalid_choice_message() -> None:
     '''
-        Initialize function to initialize
-        SQL Alchemy Engine for Reading Data
-        from PostgreSQL Database
+        Display function to display
+        invalid choice message
     '''
-    # Engine Parameters
-    username = '<POSTGRESQL_ACCOUNT_USERNAME>'
-    password = '<POSTGRESQL_ACCOUNT_PASSWORD>'
-    host = '<HOSTNAME>'
-    port = '<PORT_NUMBER>'
-    database = 'rica_metadatas'
-
-    engine = create_engine(f'postgresql://{username}:{password}@{host}:{port}/{database}')
-    return engine
-
-def init_connection() -> object:
-    '''
-        Initialize function to initialize
-        a connection to PostgreSQL Database
-        using Psycopg2 for CRUD Process
-    '''
-    conn = psycopg2.connect(
-        user='<POSTGRESQL_ACCOUNT_USERNAME>',
-        password='<POSTGRESQL_ACCOUNT_PASSWORD>',
-        host='<HOSTNAME>',
-        port='<PORT_NUMBER>',
-        database='rica_metadatas'
-    )
-    return conn
-
-def get_table_names() -> list[str]:
-    '''
-        Get function to extract all table names
-        from PostgreSQL Database
-    '''
-    list_of_table_names = []
-
-    for csv_file in glob(f'src/metadata/*.csv'):
-        csv_file = str(csv_file).replace('\\', '/')
-        table_name = str(csv_file).replace('src/metadata/', '')
-        table_name = str(table_name).replace('.csv', '')
-
-        list_of_table_names.append(table_name)
-
-    return list_of_table_names
+    os.system('cls')
+    print(f'\t\tInvalid choice! Please try again.')
+    input(f'\t\tPress any key to reload page: ')
+    os.system('cls')
