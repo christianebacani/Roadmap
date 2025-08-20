42099822400a8c8dc98ff1d36069dbca247f6352
christianebacani <christianbacani581@gmail.com>
2025-08-20 23:51:51 +0800

    MODIFY: Added new content to the function

--- Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/main/main.py
+++ Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/main/main.py
@@ -3,21 +3,22 @@
 '''
 import os
 from src.utils.utils import display_invalid_choice_message
+from src.about_the_system.about_the_system import about_the_system_page
 
 def main_page() -> None:
     '''
         Main Page Function to 
         display the main page
     '''
-    # Display welcome message
-    print(f'\tWelcome to CLI-Based CRUD System for managing my girlfriend\'s metadata!')
-    input(f'\t\tPress any key to start: ')
-    os.system('cls')
-
     while True:
         # Display header
+        print('\t\t', end='')
+        print('=' * 25)
         header = 'Main Page'
-        print(f'\t\t\t{header}\n')
+        print(f'\t\t\t{header}')
+        print('\t\t', end='')
+        print('=' * 25)
+        print()
 
         options = [
             'Start',
@@ -35,9 +36,14 @@ def main_page() -> None:
             print()
             choice = int(input(f'\t\tEnter your choice here: '))
 
-            if choice == 2:
+            if choice == 1:
                 os.system('cls')
-                # TODO: Add a function from other modules here outside the 'main' package
+                # TODO: Execute a function here from other module outsidee 'main' package
+                continue
+                
+            elif choice == 2:
+                os.system('cls')
+                about_the_system_page()
                 continue
 
             elif choice != 3:
