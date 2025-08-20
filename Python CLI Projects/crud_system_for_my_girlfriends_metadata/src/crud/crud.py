ba701d34615789d0a2b86738eec88afdb124e9a0
christianebacani <christianbacani581@gmail.com>
2025-08-20 23:51:01 +0800

    ADD: Added new module to the 'crud' package

---
+++ Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/crud/crud.py
@@ -0,0 +1,13 @@
+'''
+    CRUD Module
+'''
+def crud_page() -> None:
+    '''
+        CRUD Page function to
+        display functionality
+        about options for managing
+        metadata using CRUD
+    '''
+    while True:
+        print('\t\t', end='')
+        # TODO: Add more functionalities here...
