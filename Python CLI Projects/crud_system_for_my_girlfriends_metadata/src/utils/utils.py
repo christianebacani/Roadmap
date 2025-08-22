f98943f6ab4404e8b7ee8099d2a23191a0c8b368
christianebacani <christianbacani581@gmail.com>
2025-08-22 23:00:59 +0800

    MODIFY: Added new content to the module

--- Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/utils/utils.py
+++ Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/utils/utils.py
@@ -2,6 +2,7 @@
     Utilities Module
 '''
 import os
+from glob import glob
 
 def display_invalid_choice_message() -> None:
     '''
@@ -12,3 +13,20 @@ def display_invalid_choice_message() -> None:
     print(f'\t\tInvalid choice! Please try again.')
     input(f'\t\tPress any key to reload page: ')
     os.system('cls')
+
+
+def get_the_list_of_table_names() -> list[str]:
+    '''
+        Get function to get the
+        list of table names by 
+        reading the file names
+        from 'metadata' directory
+    '''
+    result = []
+
+    for csv_file in glob(f'src/metadata/*.csv'):
+        csv_file = str(csv_file).replace('\\', '/')
+        table_name = str(csv_file).replace('src/metadata/', '').replace('.csv', '')
+        result.append(table_name)
+
+    return result
