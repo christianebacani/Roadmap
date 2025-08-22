dbea3649688171c385a9d5d3583f8a1bedd46021
christianebacani <christianbacani581@gmail.com>
2025-08-22 22:57:17 +0800

    MODIFY: Added new content to the function 'crud_page'

--- Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/crud/crud.py
+++ Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/crud/crud.py
@@ -1,6 +1,11 @@
 '''
     CRUD Module
 '''
+import os
+from src.utils.utils import display_invalid_choice_message
+from src.crud.insert_data import insert_data_page
+from src.crud.read_data import read_data_page
+
 def crud_page() -> None:
     '''
         CRUD Page function to
@@ -9,5 +14,50 @@ def crud_page() -> None:
         metadata using CRUD
     '''
     while True:
+        # Display header
         print('\t\t', end='')
-        # TODO: Add more functionalities here...
+        print('=' * 25)
+        header = 'CRUD Page'
+        print(f'\t\t\t{header}')
+        print('\t\t', end='')
+        print('=' * 25)
+        print()
+
+        options = [
+            'Create Table',
+            'Insert data',
+            'Read data',
+            'Update data',
+            'Delete data',
+            'Exit'
+        ]
+
+        # Display options
+        for number, option in enumerate(options):
+            number += 1
+            print(f'\t\t{number}.) {option}')
+
+        try:
+            choice = int(input(f'\n\t\tEnter your choice here: '))
+
+            if choice == 2:
+                os.system('cls')
+                insert_data_page()
+                continue
+
+            elif choice == 3:
+                os.system('cls')
+                read_data_page()
+                continue
+
+            elif choice != 6:
+                display_invalid_choice_message()
+                continue
+            
+            else:
+                os.system('cls')
+                break
+
+        except ValueError:
+            display_invalid_choice_message()
+            continue
