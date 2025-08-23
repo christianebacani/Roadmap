aee203b4059bef44bae35204d770a1db60d62780
christianebacani <christianbacani581@gmail.com>
2025-08-23 23:19:49 +0800

    MODIFY: Added function inside the function 'insert_data' and new functionalities from the fucntion 'insert_data'

--- Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/crud/insert_data.py
+++ Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/crud/insert_data.py
@@ -5,16 +5,42 @@ import os
 import pandas as pd
 from src.utils.utils import display_invalid_choice_message
 from src.utils.utils import get_the_list_of_table_names
+from src.utils.utils import init_connection
 
 def insert_data(table_name: str) -> None:
     '''
         Insert data function to insert
         new data from the chosen table
     '''
-    dataframe = pd.read_csv(f'src/metadata/{table_name}.csv')
-    columns = list(dataframe.columns)
+    def init_sql_command_for_data_insertion(table_name: str, inserted_data: dict[str, str]) -> str:
+        '''
+            Initialize function to initialize
+            SQL Command for Data Insertion
+        '''
+        columns = ', '.join(list(insert_data.keys()))
+        command = f'INSERT INTO {table_name} ({columns})'
+        
+        values = []
+    
+        for value in list(inserted_data.values()):
+            # Check if the value can be typecasted to integer and float
+            try:
+                int(value)
+                float(value)
+                values.append(value)
+
+            # We need to make sure when inserting string/date/time/datetime datatype values we use open and close quotation marks
+            except ValueError:
+                values.append(f'\'{value}\'')
+        
+        values = ', '.join(values)
+        command = command + ' ' + 'VALUES' + '(' + values + ')'
+        
+        return command
 
     while True:
+        columns = list(pd.read_csv(f'src/metadata/{table_name}.csv').columns)
+
         # Display header
         print(f'\t\t', end='')
         print('=' * 28)
@@ -25,16 +51,17 @@ def insert_data(table_name: str) -> None:
         print()
 
         # Dictionary to store the new data as a value and column name as a key
-        new_data = {}
+        inserted_data = {}
 
         for column in columns:
-            new_data[column] = None
+            inserted_data[column] = None
 
         print(f'\t\tTable Name: {table_name}')
         
         inserted_data_not_confirm = False
         invalid_confirmation = False
-    
+
+        # Ask for the data to be inserted to the given table
         for column in columns:
             data = input(f'\t\tEnter the new data for {column} column: ')
             confirm_inserted_data = input(f'\t\tDid you enter the correct data (Y/N)?: ').strip().upper()
@@ -46,12 +73,62 @@ def insert_data(table_name: str) -> None:
             if confirm_inserted_data != 'Y':
                 invalid_confirmation = True
                 break
-        
+            
+            if data == '':
+                inserted_data[column] = None
+
+            else:
+                inserted_data[column] = data
+
         if inserted_data_not_confirm:
             os.system('cls')
             continue
+        
+        if invalid_confirmation:
+            os.system('cls')
+            print(f'\t\tInvalid confirmation! Please try again.')
+            input(f'\t\tPress any key to reload the page: ')
+            os.system('cls')
+            continue
+        
+        # Display header
+        os.system('cls')
+        print(f'\t\t', end='')
+        print('=' * 28)
+        header = 'Inserted Data'
+        print(f'\t\t\t{header}')
+        print(f'\t\t', end='')
+        print('=' * 28)
+        print()
+        
+        # Display table name and the inserted data per column
+        print(f'\t\tTable Name: {table_name}')
 
-        # TODO: Add more functionalities here...
+        for column, data in inserted_data.items():
+            print(f'\t\t{column}: {data}')
+        
+        input(f'\n\t\tPress any key to insert the given data: ')
+        
+        try:
+            conn = init_connection() # Initialize a connection to the PostgreSQL Database using Pyscopg2
+            cursor = conn.cursor() # Initialize a cursor from the established connection
+            command = init_sql_command_for_data_insertion(table_name, inserted_data)
+        
+            cursor.execute(command)
+            conn.commit()
+
+            dataframe = pd.read_sql(f'SELECT * FROM {table_name}', conn)
+            target_filepath = f'src/metadata/{table_name}.csv'
+            dataframe.to_csv(target_filepath, index=False)
+
+            # TODO: Add more functionalities here...
+
+        except Exception as error_message:
+            os.system('cls')
+            print(f'\t\tError: {error_message}')
+            input(f'\t\tPress any key to reload page: ')
+            os.system('cls')
+            continue
 
 def insert_data_page() -> None:
     '''
@@ -98,7 +175,7 @@ def insert_data_page() -> None:
             
             elif (choice >= 1 and choice <= (len(table_names) + 1)):
                 os.system('cls')
-                # TODO: Execute a new function here from the same module 'insert_data.py'
+                insert_data(table_names[choice - 1])
                 continue
 
             else:
