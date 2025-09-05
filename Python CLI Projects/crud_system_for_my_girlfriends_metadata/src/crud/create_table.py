4df246b4e95e346ba4aa42b24592252e43b01340
christianebacani <christianbacani581@gmail.com>
2025-09-05 21:09:13 +0800

    MODIFY: Modify the component inside the function 'create_primary_keys' to ensure no bugs

--- Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/crud/create_table.py
+++ Python CLI Projects/crud_system_for_my_girlfriends_metadata/src/crud/create_table.py
@@ -33,7 +33,7 @@ def create_table_name() -> str:
         os.system('cls')
         return table_name
 
-def get_the_total_number_of_columns(table_metadata: dict[str, str | int | list]) -> int:
+def get_the_total_number_of_columns(table_metadata: dict[str, str | int | dict[str, str]]) -> int:
     '''
         Get function to get the
         total number of columns
@@ -79,7 +79,7 @@ def get_the_total_number_of_columns(table_metadata: dict[str, str | int | list])
             display_invalid_choice_message()
             continue
 
-def create_primary_keys(table_metadata: dict[str, str | int | list]) -> list[str]:
+def create_primary_keys(table_metadata: dict[str, str | int | dict[str, str]]) -> dict[str, str]:
     '''
         Create function to create
         primary keys for the new table
@@ -106,7 +106,21 @@ def create_primary_keys(table_metadata: dict[str, str | int | list]) -> list[str
 
             if number_of_primary_keys == 0:
                 os.system('cls')
-                return 0
+                return {}
+
+            if number_of_primary_keys > table_metadata['Number of Columns']:
+                display_invalid_choice_message()
+                continue
+            
+            confirm_number_of_primary_keys = input(f'\t\tDid you enter the correct number of primary keys (Y/N)?: ').strip().upper()
+
+            if confirm_number_of_primary_keys == 'N':
+                os.system('cls')
+                continue
+
+            if confirm_number_of_primary_keys != 'Y':
+                display_invalid_choice_message()
+                continue
 
             # Display header
             os.system('cls')
@@ -124,14 +138,15 @@ def create_primary_keys(table_metadata: dict[str, str | int | list]) -> list[str
                 'CHAR',
                 'FLOAT'
             ]
-            primary_keys_and_datatype = {}
-
+            
             primary_key_not_confirm = False
             invalid_confirmation = False
+            invalid_character_size = False
+            invalid_floating_point_size = False
 
             for _ in range(number_of_primary_keys):
-                primary_key = input(f'\t\tEnter the primary key name: ')
-                confirm_primary_key = input(f'\t\tDid you enter the correct primary key (Y/N)?: ').strip().upper()
+                primary_key = input(f'\t\tEnter your primary key here: ')
+                confirm_primary_key = input(f'\t\tDid you enter the correct primary key (Y/N)?; ').strip().upper()
 
                 if confirm_primary_key == 'N':
                     primary_key_not_confirm = True
@@ -140,19 +155,35 @@ def create_primary_keys(table_metadata: dict[str, str | int | list]) -> list[str
                 if confirm_primary_key != 'Y':
                     invalid_confirmation = True
                     break
-
+                
                 print()
 
                 for number, datatype in enumerate(datatypes):
-                    print(f'\t\t{number}.) {datatype}')
-                
-                print()
-                chosen_datatype = input(f'\t\tChoose the datatype of {primary_key} primary: ')
+                    number += 1
+                    print(f'\t\t\t{number}.) {datatype}')
 
-                if chosen_datatype <= 0:
-                    display_invalid_choice_message()
-                    continue
-                    
+                chosen_datatype = int(input(f'\t\tEnter the datatype of {primary_key} primary key: '))
+
+                if datatypes[chosen_datatype - 1] in ['VARCHAR', 'CHAR']:
+                    size = input(f'\t\tEnter the size of character from {primary_key} primary key: ')
+                
+                elif datatypes[chosen_datatype - 1] == 'FLOAT':
+                    size = input(f'\t\tEnter the floating-point size of {primary_key} primary key: ')
+                    decimal_digits = input(f'\t\tEnter the decimal digits of {primary_key} primary key: ')
+
+                else:
+                    pass
+
+                if (datatypes[chosen_datatype - 1] in ['VARCHAR', 'CHAR']) and (int(size) <= 0 or int(size) > 255):
+                    invalid_character_size = True
+                    break
+                
+                if (datatype[chosen_datatype - 1] == 'FLOAT') and (int(size) <= 0 or int(size) > 32):
+                    invalid_floating_point_size = True
+                    break
+                
+                # TODO: Add more validations here...
+
             if primary_key_not_confirm:
                 os.system('cls')
                 continue
@@ -160,11 +191,43 @@ def create_primary_keys(table_metadata: dict[str, str | int | list]) -> list[str
             if invalid_confirmation:
                 display_invalid_choice_message()
                 continue
-                        
-        except ValueError:
-            display_invalid_choice_message()
+
+            if invalid_character_size:
+                os.system('cls')
+                print(f'\t\tInvalid character size! Please try again.')
+                input(f'\t\tPress any key to reload page: ')
+                os.system('cls')
+                continue
+                
+            if invalid_floating_point_size:
+                os.system('cls')
+                print(f'\t\tInvalid floating-point size! Please try again.')
+                input(f'\t\tPress any key to reload page: ')
+                os.system('cls')
+                continue
+
+        except Exception as error_message:
+            print(f'\t\tError message: {error_message}')
             continue
 
+def create_non_key_columns(table_metadata: dict[str, str | int | dict[str, str]]) -> dict[str, str]:
+    '''
+        Create function to
+        create non-key columns
+        for new table
+    '''
+    while True:
+        # Display header
+        print(f'\t\t', end='')
+        print('=' * 30)
+        header = 'Creating Table'
+        print(f'\t\t\t{header}')
+        print(f'\t\t', end='')
+        print('=' * 30)
+        print()
+
+        print(f'\t\tTable Name: {table_metadata['Table Name']}')
+        
 def create_table_page() -> None:
     '''
         Create Table Page function
@@ -194,11 +257,12 @@ def create_table_page() -> None:
         table_metadata = {
             'Table Name': '',
             'Number of Columns': 0,
-            'Primary Keys': [],
-            'Non-Key Columns': []
+            'Primary Keys': {},
+            'Non-Key Columns': {}
         }
 
         os.system('cls')
         table_metadata['Table Name'] = create_table_name()
         table_metadata['Number of Columns'] = get_the_total_number_of_columns(table_metadata)
+        table_metadata['Primary Keys'] = create_primary_keys(table_metadata)
         break
