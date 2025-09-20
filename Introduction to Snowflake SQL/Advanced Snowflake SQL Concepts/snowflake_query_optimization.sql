f6f9eae818d36a31a34df6427bbe45ab20f7073c
christianebacani <christianbacani581@gmail.com>
2025-09-20 15:33:51 +0800

    feat: Added new table to the sql file and inserted new data

--- Introduction to Snowflake SQL/Advanced Snowflake SQL Concepts/snowflake_query_optimization.sql
+++ Introduction to Snowflake SQL/Advanced Snowflake SQL Concepts/snowflake_query_optimization.sql
@@ -15,11 +15,38 @@ CREATE OR REPLACE TABLE
     address TEXT
     );
 
+-- Shop types table
+CREATE OR REPLACE TABLE
+    shop_types (
+    shop_type_id NUMBER(38, 0) PRIMARY KEY,
+    shop_type VARCHAR(255)
+    );
+
+-- Shops table
+CREATE OR REPLACE TABLE
+    shops (
+    shop_id NUMBER(38, 0) PRIMARY KEY,
+    shop_type_id NUMBER(38, 0),
+    shop VARCHAR(255),
+    address TEXT
+    );
+
 -- Product categories table
 CREATE OR REPLACE TABLE
     product_categories (
     product_category_id NUMBER(38, 0) PRIMARY KEY,
     product_category VARCHAR(255)
+    );
+
+-- Products table
+CREATE OR REPLACE TABLE
+    products (
+    product_id NUMBER(38, 0) PRIMARY KEY,
+    restaurant_id NUMBER(38, 0),
+    shop_id NUMBER(38, 0),
+    product_category_id NUMBER(38, 0),
+    product VARCHAR(255),
+    price NUMBER(11, 2)
     );
 
 
@@ -30,7 +57,29 @@ INSERT INTO
     address
     )
     VALUES
-    (1, 'Mcdonalds Luisita', 'Luisita Access Road 2300 Tarlac City Tarlac');
+    (1, 'Mang Inasal - Robinsons Tarlac', 'Robinsons Luisita McArthur Highway Barangay San Miguel 2nd District');
+
+INSERT INTO
+    shop_types (
+    shop_type_id,
+    shop_type
+    )
+    VALUES
+    (1, 'Bakery'),
+    (2, 'Bakery & Sweets'),
+    (3, 'Health & Wellness'),
+    (4, 'Pharmacy'),
+    (5, 'Supermarket');
+
+INSERT INTO
+    shops (
+    shop_id,
+    shop_type_id,
+    shop,
+    address
+    )
+    VALUES
+    (1, 2, 'Jelexie (San Miguel)', '28 McArthur Highway Barangay San Miguel 53rd District');
 
 INSERT INTO
     product_categories (
@@ -43,4 +92,4 @@ INSERT INTO
 
 SELECT *
 FROM
-    product_categories;
+    products;
